d36d092fafd5c3f4661b29280ddcd16ef3cfe172
smileU <1484249625@qq.com>
2016-11-15 09:35:05 +0800

    Update mip-changyan.js

--- mip-changyan/extensions/mip-changyan.js
+++ mip-changyan/extensions/mip-changyan.js
@@ -1,24 +1,30 @@
+/**
+* 畅言插件
+* @author smileU
+* @version 1.0
+*/
+define(function(require) {
+	var $ = require('zepto');
+	
+	var customElement = require('customElement').create();
+	
+	customElement.prototype.createdCallback = function () {
+		
+		var _element = this.element;
 
-define("mip-changyan", ["require", "zepto", "customElement"], function(t) {
-	function n() {
-		var e = this.element;
-		if(!e.isRender) {
-			e.isRender = !0;
-			var appid = e.getAttribute("appid"),
-			conf = e.getAttribute("conf"),
-			a = i(e),
-			h = ['<script type="text/javascript">',
-			'var _hmt = _hmt || []; (function() {var hm = document.createElement("script"); hm.src = "//changyan.sohu.com/upload/changyan.js?appid=' + appid + '&conf=' + conf + '";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();',
-			"</script>"];	
-			a.append(h.join(""))
-		}
+		var appid = _element.getAttribute('appid'),
+		conf = _element.getAttribute("conf"),
+
+		var $_element = $(_element);
+		var html = [
+		    '<script type="text/javascript">',
+			'var _hmt = _hmt || []; (function() { var hm = document.createElement("script"); hm.src = "//changyan.sohu.com/upload/changyan.js?appid=' + appid + '&conf=' + conf + '";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();',
+		    '</script>'
+		];
+
+		$_element.append(html.join(''));
 	}
-	var i = t("zepto"),
-	s = t("customElement").create();
-	return s.prototype.init = function() {
-		this.createdCallback = n
-	},s 
-});
-require(["mip-changyan"], function(t) {
-    MIP.registerMipElement("mip-changyan", t);   
+
+	return customElement;
+	
 });

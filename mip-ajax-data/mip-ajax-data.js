9678e655a68b44b0e6b563c0340ce4a6846f7818
wongfeng <289047960@qq.com>
2016-11-15 13:32:34 +0800

    mip-ajax-data 用来触发元素ajax异步加载数据到指定容器，也可以下拉加载

---
+++ mip-ajax-data/mip-ajax-data.js
@@ -0,0 +1,62 @@
+/**
+ * @file mip-ajax-data 组件
+ * @author Jobs Fan
+ */
+
+define(function (require) {
+
+    var customElement = require('customElement').create();
+    var $ = require('zepto');
+    
+    
+    /**
+     * 绑定事件
+     */
+    function bindEven(element,params,once) {
+        if (params["action"] != 'roll'){
+        	$(element).on(params["action"],"span",function(){
+        		var url = once ? params["url"] : params["url"].replace("[markplaceholder]",$(element).attr('mip-ajax-mark'));
+    			$.getJSON(url,function(result){
+                	$("#"+params["containerid"]).append(result['html']);
+                	if (result['length'] < params["length"] || once){
+                		$(element).remove();
+                	}else{
+                		$(element).attr('mip-ajax-mark',result['mip-ajax-mark']);
+                	}
+                });
+        	});
+        }else{
+        	var allow = true;
+        	$(window).bind("scroll",function(){
+        		if (typeof($(element).get(0)) == 'undefined') return false;
+        		if (allow && $(window).height() + window.pageYOffset >= $(element).offset().top){
+        			allow = false;
+        			var url = once ? params["url"] : params["url"].replace("[markplaceholder]",$(element).attr('mip-ajax-mark'));
+        			$.getJSON(url,function(result){
+                    	$("#"+params["containerid"]).append(result['html']);
+                    	if (result['length'] < params["length"] || once){
+                    		$(element).remove();
+                    	}else{
+                    		$(element).attr('mip-ajax-mark',result['mip-ajax-mark']);
+                    		allow = true;
+                    	}
+                    });
+        		}
+        	});
+        }
+    }
+    
+    /**
+     * 构造元素，只会运行一次
+     */
+    customElement.prototype.build = function () {
+    	var self = this;
+        var element = this.element;
+        
+        var params = JSON.parse($(element).attr('mip-ajax-params').replace(/'/g, "\""));
+        
+        bindEven(element,params,typeof($(element).attr('mip-ajax-mark')) == 'undefined');
+    };
+
+    return customElement;
+});

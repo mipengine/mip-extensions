67c06c53db59b564fed323d038f9ad3895845131
phpsupai <zhchhu0@163.com>
2016-11-17 14:41:02 +0800

    帝国cms 修改-文件格式等
    添加作者,js通过fecs检测,修改描述

--- mip-ecms/mip-ecms.js
+++ mip-ecms/mip-ecms.js
@@ -4,58 +4,55 @@
  * @date: 2016-11-16
  * @file: mip-ecms.js
  */
-define(function(require) {
+define(function (require) {
     var $ = require('zepto');
     var customElement = require('customElement').create();
 
     // 页面交互效果
     var effects = {
-        bdshijian: function() {
-            $(".favorite").on('click', function(event) {
+        bindEvents: function () {
+            $('.favorite').on('click', function (event) {
                 event.preventDefault();
                 this.zan();
             });
         },
         // 文章点赞
-        zan: function() {
+        zan: function () {
             if ($(this).hasClass('done')) {
                 return false;
-            } else {
-                $(this).addClass('done');
-                var id = $(this).data("id"),
-                    classid = $(this).data('classid'),
-                    action = $(this).data('action'),
-                    rateHolder = $(this).children('.count');
-                var ajax_data = {
-                    classid: classid,
-                    id: id,
-                    dotop: 1,
-                    doajax: 1,
-                    ajaxarea: 'diggnum'
-                };
-                $.get("/e/public/digg/index.php", ajax_data,
-                    function(data) {
-                        var dingnumr = data.substring(0, data.indexOf("|"));
-                        if (dingnumr != "") {
-                            $(rateHolder).html(dingnumr);
-                        } else {
-                            alert("你已经提交过了");
-                        }
-
-                    });
-                return false;
             }
+            $(this).addClass('done');
+            var id = $(this).data('id');
+            var classid = $(this).data('classid');
+            var rateHolder = $(this).children('.count');
+            var ajaxData = {
+                classid: classid,
+                id: id,
+                dotop: 1,
+                doajax: 1,
+                ajaxarea: 'diggnum'
+            };
+            $.get('/e/public/digg/index.php', ajaxData,
+                function (data) {
+                    var dingnumr = data.substring(0, data.indexOf('|'));
+                    if (!dingnumr) {
+                        $(rateHolder).html(dingnumr);
+                    }
+                    else {
+                        alert('你已经提交过了');
+                    }
+
+                });
+            return false;
         },
         // 加载绑定
-        init: function() {
-            this.bdshijian();
+        init: function () {
+            this.bindEvents();
         }
     };
 
-    customElement.prototype.build = function() {
+    customElement.prototype.build = function () {
         effects.init();
     };
-
     return customElement;
-
 });

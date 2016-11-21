47f721582f962166e5a69845aeb26e9c493d2a7a
missjixiang <1334935207@qq.com>
2016-11-21 17:25:58 +0800

    第三方广告扩展插件

---
+++ mip-jx-ad/README.md
@@ -0,0 +1,22 @@
+# mip-jx-ad
+
+mip-jx-ad 第三方聚效广告扩展组件
+
+标题|内容
+----|----
+类型|通用，定制
+支持布局|responsive,fixed-height,fill,container,fixed
+所需脚本|https://mipcache.bdstatic.com/static/v1/mip-jx-ad/mip-jx-ad.js
+
+## 示例
+
+用于投放聚效第三方广告的扩展组件
+```html
+<mip-ad 
+     type="ad-qwang"
+     unionId="u2816363"
+	 cpro_pswidth="309"
+	 cpro_psheight="250"
+     >
+</mip-ad>
+```
--- mip-jx-ad/mip-jx-ad.js
+++ mip-jx-ad/mip-jx-ad.js
@@ -6,13 +6,13 @@
  */
 
 define(function (require) {
-        var appendtxt = $('<a style="display:none!important" id="tanx-a-mm_34618856_4222645_40104258"></a>');
+        var appendtxt = $('<a style="display:none!important" id="tanx-a-mm_34618856_4222645_14288433"></a>');
         $(document.body).append(appendtxt);
         var node = document.createElement('script');
         node.type = 'text/javascript';
-        node.src = 'http://p.tanx.com/ex?i=mm_34618856_4222645_40104258';
+        node.src = 'http://p.tanx.com/ex?i=mm_34618856_4222645_14288433';
         node.async = 'true';
-        node.id = 'tanx-s-mm_34618856_4222645_40104258';
+        node.id = 'tanx-s-mm_34618856_4222645_14288433';
         var tanxh = document.getElementsByTagName('head')[0];
         if (tanxh) {
             tanxh.insertBefore(node, tanxh.firstChild);

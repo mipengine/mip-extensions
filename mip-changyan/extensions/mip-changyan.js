bcbf7a8905b0821d4816264cf8ce16a7727f536d
smileU <1484249625@qq.com>
2016-11-21 17:27:07 +0800

    Update mip-changyan.js

--- mip-changyan/extensions/mip-changyan.js
+++ mip-changyan/extensions/mip-changyan.js
@@ -14,15 +14,13 @@ define(function(require) {
 
 		var appid = _element.getAttribute('appid'),
 		conf = _element.getAttribute("conf");
-
-		var $_element = $(_element);
-		var html = [
-		    '<script type="text/javascript">',
-			'var _hmt = _hmt || []; (function() { var hm = document.createElement("script");hm.id = "changyan_mobile_js";hm.charset = "utf-8";hm.src = "//changyan.sohu.com/upload/changyan.js?appid=' + appid + '&conf=' + conf + '";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();',
-		    '</script>'
-		];
-
-		$_element.append(html.join(''));
+		
+		var width = window.innerWidth || document.documentElement.clientWidth; 
+		if (width < 960) { 
+			window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); 
+		} else { 
+			var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("http://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); 
+		} 
 	}
 
 	return customElement;

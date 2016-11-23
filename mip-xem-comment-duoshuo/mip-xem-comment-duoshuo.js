1b93f9a72ea73fdeb93f5c75b9361da7803d4bc6
mimipigs <mimipigs@qq.com>
2016-11-23 16:04:27 +0800

    已修改代码并已通过fecs规范

--- mip-xem-comment-duoshuo/mip-xem-comment-duoshuo.js
+++ mip-xem-comment-duoshuo/mip-xem-comment-duoshuo.js
@@ -1,29 +1,30 @@
 /**
-* 多说插件
-* @exports modulename
-* @author mip-support@xem.com
+* @file 多说评论框通用代码稳定版组件
+* @author mip-support@hzxem.com
 * @version 1.0
-* @copyright 2016 xxx.com, Inc. All Rights Reserved
+* @copyright 2016 hzxem.com, Inc. All Rights Reserved
 */
 
 define(function (require) {
     var $ = require('zepto');
-
-     var customElement = require('customElement').create();
-     customElement.prototype.build = function () {
-       var _element = this.element;
-        
-        var short_name = _element.getAttribute('short_name');
-
-        var $_element = $(_element);
+    var customElement = require('customElement').create();
+    customElement.prototype.build = function () {
+        var elem = this.element;
+        var shortname = elem.getAttribute('short_name');
+        var $elem = $(elem);
         var html = [
             '<script type="text/javascript">',
-                'var duoshuoQuery={short_name:"'+short_name+'"};(function(){var ds=document.createElement(\'script\');ds.type=\'text/javascript\';ds.async=true;ds.src=(document.location.protocol==\'https:\'?\'https:\':\'http:\')+\'//static.duoshuo.com/embed.js\';ds.charset=\'UTF-8\';(document.getElementsByTagName(\'head\')[0]||document.getElementsByTagName(\'body\')[0]).appendChild(ds)})();',
+            'var duoshuoQuery={short_name:"' + shortname + '"};',
+            '(function() {var ds=document.createElement(\'script\');',
+            'ds.type=\'text/javascript\';',
+            'ds.async=true;',
+            'ds.src=(document.location.protocol==\'https:\'?\'https:\':\'http:\')+\'//static.duoshuo.com/embed.js\';',
+            'ds.charset=\'UTF-8\';',
+            '(document.getElementsByTagName(\'head\')[0]||',
+            'document.getElementsByTagName(\'body\')[0]).appendChild(ds)})();',
             '</script>'
         ];
-
-        $_element.append(html.join(''));
-    }
-
+        $elem.append(html.join(''));
+    };
     return customElement;
 });

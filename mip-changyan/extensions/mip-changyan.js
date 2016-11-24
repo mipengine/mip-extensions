1438d9398b1115127fa0d879e37d80112c8b4a23
smileU <1484249625@qq.com>
2016-11-24 17:00:09 +0800

    Update mip-changyan.js

--- mip-changyan/extensions/mip-changyan.js
+++ mip-changyan/extensions/mip-changyan.js
@@ -9,6 +9,19 @@ define(function(require) {
 	var customElement = require('customElement').create();
 	
 	customElement.prototype.createdCallback = function () {
+		/*var _element = this.element;
+
+		var appid = _element.getAttribute('appid'),
+		conf = _element.getAttribute("conf");
+
+		var $_element = $(_element);
+		var html = [
+		    '<script type="text/javascript">',
+			'var _hmt = _hmt || []; (function() { var hm = document.createElement("script"); hm.src = "//changyan.sohu.com/upload/changyan.js?appid=' + appid + '&conf=' + conf + '";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s); })();',
+		    '</script>'
+		];
+
+		$_element.append(html.join(''));*/
 		
 		var _element = this.element;
 

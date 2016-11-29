1a410c56cfbaa9d6f4116352356e471174fe8d20
weiling0626 <741999251@qq.com>
2016-11-29 16:58:39 +0800

    Update mip-ad-random.js

--- mip-ad-random/mip-ad-random.js
+++ mip-ad-random/mip-ad-random.js
@@ -1,43 +1,46 @@
 /**
+ * @file FECS test file
+ * @author cgzero(cgzero@cgzero.com)
  * 随机广告插件
  * @version 1.0.0
  */
 define(function (require) {
-    var $ = require("zepto");
-    var customElement = require("customElement").create();
+    var $ = require('zepto');
+    var customElement = require('customElement').create();
+    var t = this.element;
     function e() {
-        var t = this.element;
         if (!t.isRender) {
             t.isRender = !0;
-            function GetPosition() {                  
-                    //随机要放入广告的大位置
-                    var a = $("mip-ad-random").data("parents"); 
-                    var arry1 = $('.' + a);
-                    var arry_length1 = arry1.length;
-                    var rand_num = Math.ceil(Math.random() * arry_length1) - 1;
-                    var rank = arry1[rand_num];
-                    //随机子元素放入广告
-                    var r_children = $(rank).children();
-                    var arry_length2 = r_children.length;
-                    var rand_num2 = Math.ceil(Math.random() * arry_length2) - 1;
-                    var rank2 = r_children[rand_num2];
-                    var adSrc = t.getAttribute('ad-src'); //广告图片路径
-                    var adHref = t.getAttribute('ad-href'); //广告链接路径
-                    var adTitle = t.getAttribute('ad-title'); //广告标题
-                    var adClass = t.getAttribute('ad-class'); //广告样式名
-                    var adWidth = t.getAttribute('ad-width');//广告所占宽度
-                    var adHeight = t.getAttribute('ad-height');//广告所占高度                   
-                    //判断：若大位置没有子元素，就在其里面添加广告，若大位置有子元素，在随机子元素的后面添加广告
-                    var advet='<mip-ad layou="reponsive" width="' +adWidth+ '" height="'+adHeight+'" type="ad-com" tpl="oneImg" class="'+adClass+'" href="'+adHref+'" data-size="1242 180" src="'+adSrc+'" data-title="'+adTitle+'"></mip-ad>';
-                    if (arry_length2 == 0) {
-                        $(rank).append(advet);
-                    } else {
-                        $(rank2).after(advet);
-                    }
-                }
-                GetPosition();
+            getadress();
+        }
+    }
+    function getadress() {
+        var a = $('mip-ad-random').data('parents');
+        var arry1 = $('.' + a);
+        var arrylength1 = arry1.length;
+        var randnum = Math.ceil(Math.random() * arrylength1) - 1;
+        var rank = arry1[randnum];
+        var rchildren = $(rank).children();
+        var arrylength2 = rchildren.length;
+        var randnum2 = Math.ceil(Math.random() * arrylength2) - 1;
+        var rank2 = rchildren[randnum2];
+        var adsrc = t.getAttribute('ad-src');
+        var adhref = t.getAttribute('ad-href');
+        var adTitle = t.getAttribute('ad-title');
+        var adClass = t.getAttribute('ad-class');
+        var adWidth = t.getAttribute('ad-width');
+        var adHeight = t.getAttribute('ad-height');
+        var advet = '<mip-ad layou="reponsive" width="' + adWidth + '" height="' + adHeight + '" '
+        + 'type="ad-com" tpl="oneImg" class="' + adClass + '" href="' + adhref + '" '
+        + 'data-size="1242 180" src="' + adsrc + '" data-title="' + adTitle + '"></mip-ad>';
+        if (arrylength2 === 0) {
+            $(rank).append(advet);
+        }
+        else {
+            $(rank2).after(advet);
         }
     }
     customElement.prototype.build = e;
     return customElement;
-})
+}
+);

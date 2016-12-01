276d62d1befb1a4f8749b063c2f7b5255f5c6c6d
huanghuixin <274919800@qq.com>
2016-12-01 13:49:22 +0800

    代码格式规范

--- mip-ad-video/mip-ad-video.js
+++ mip-ad-video/mip-ad-video.js
@@ -1,61 +1,65 @@
+/**
+ * @file mip-ad-video 带有广告的视频播放组件主文件
+ * @author vodjk
+ */
+
 define(function (require) {
-    // mip 组件开发支持 zepto
     var $ = require('zepto');
 
     var customElem = require('customElement').create();
 
-
-    /* 生命周期 function list，根据组件情况选用，（一般情况选用 build、firstInviewCallback） start */
-    // build 方法，元素插入到文档时执行，仅会执行一次
+    /**
+     * prerenderAllowed
+     */
     customElem.prototype.prerenderAllowed = function () {
         // this.element 可取到当前实例对应的 dom 元素
         var $element = $(this.element);
-        var adSrc = $element.attr("ad-src"),
-            targetSrc = $element.attr("target-src"),
-            adTime = $element.attr("ad-time");
+        var adSrc = $element.attr('ad-src');
+        var targetSrc = $element.attr('target-src');
+        var adTime = $element.attr('ad-time');
 
-        //初始化播放器
-        var video = document.createElement("video");
-        //广告剩余时间提醒
-        var span_adTime = document.createElement("span");
-        span_adTime.innerHTML = adTime + "";
+        //  初始化播放器
+        var video = document.createElement('video');
+        //  广告剩余时间提醒
+        var spanAdTime = document.createElement('span');
+        spanAdTime.innerHTML = adTime + '';
 
-        //设置一些video的属性
+        //  初始化video的属性
         $(video).attr({
-            "playsinline": "",
-            "webkit-playsinline": "",
-            "autoPlay": ""
+            'playsinline': '',
+            'webkit-playsinline': '',
+            'autoPlay': ''
         });
 
         $element[0].appendChild(video);
 
-        //如果有广告 则播放广告
+        //  如果有广告 则播放广告
         if (adSrc) {
-            $element[0].appendChild(span_adTime);
+            $element[0].appendChild(spanAdTime);
             video.src = adSrc;
 
-            //当开始播放 或者点击播放的时候 进行广告倒计时
+            //  当开始播放 或者点击播放的时候 进行广告倒计时
             video.onplay = video.onclick = function () {
                 video.onplay = video.onclick = null;
-                var timeId = setInterval(()=> {
+                var timeId = setInterval(function () {
                     adTime--;
-                    //刷新显示的广告秒
-                    span_adTime.innerHTML = adTime;
-                    //广告播放完毕
-                    if(adTime <= 0){
-                        span_adTime.style.display = "none";
+                    //  刷新显示的广告秒
+                    spanAdTime.innerHTML = adTime;
+                    //  广告播放完毕
+                    if (adTime <= 0) {
+                        spanAdTime.style.display = 'none';
                         clearInterval(timeId);
-                        video.src = targetSrc;//替换播放源
+                        video.src = targetSrc;//    替换播放源
 
-                        video.setAttribute("controls", "");
+                        video.setAttribute('controls', '');
                         video.load();
                         video.play();
                     }
                 }, 1000);
             };
-        } else {//否则直接播放内容
+        } else {  //  否则直接播放内容
             video.src = targetSrc;
-            video.setAttribute("controls", "");
+            video.setAttribute('controls', '');
         }
     };
 

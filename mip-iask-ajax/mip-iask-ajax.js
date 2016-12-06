e53c36b37de90703971390e968b12ca0154a6aa1
hejieye <ishejieye@126.com>
2016-12-06 19:48:54 +0800

    Update mip-iask-ajax.js

--- mip-iask-ajax/mip-iask-ajax.js
+++ mip-iask-ajax/mip-iask-ajax.js
@@ -9,34 +9,37 @@ define("mip-iask-ajax",['require', 'customElement', 'zepto'],function(require) {
 	
 	var $ = require('zepto');
 	var customElem = require('customElement').create();
-	var ajaxPost = function(url,params,isLogin,callback) {
+	var checkLogin = function(url,params,isLogin,callback) {
 		var json = eval("("+params+")");
-		var flag = false;
 		if(isLogin) {
 			// 验证是否登录
-			var checkLoginUrl='/checkLogin?m='+Math.random();
+			var checkLoginUrl='http://m.iask.sina.com.cn/checkLogin?m='+Math.random();
 			$.get(checkLoginUrl,function(e){
 				if(e==null || e =='null'){
 					flag = true;
 					// 跳转到登录页面
 					thisHref=window.location.href;
-               		window.location.href="/login?source=" + thisHref;
+               		window.location.href="http://m.iask.sina.com.cn/login?source=" + thisHref;
                		return;
+				}else{
+					ajaxPost(url,json,callback);
 				}
 			});
-		}
-		if(!flag) {
-			$.post(url, json,function(data){
-				var res = eval("("+data+")");
-				if(res.succ=='Y' && res.jsonData != '0') {
-					// 成功
-					eval(callback);
-				} else {
-					// 失败
-				}
-			});
+		} else {
+			ajaxPost(url,json,callback);
 		}
 		
+	};
+	var ajaxPost = function(url,params,callback) {
+		$.post(url, params,function(data){
+			var res = eval("("+data+")");
+			if(res.succ=='Y' && res.jsonData != '0') {
+				// 成功
+				eval(callback);
+			} else {
+				// 失败
+			}
+		});
 	};
 	
 	// build 方法，元素插入到文档时执行，仅会执行一次
@@ -49,10 +52,10 @@ define("mip-iask-ajax",['require', 'customElement', 'zepto'],function(require) {
 		var click = $(elem).attr('click');
 		var callback = $(elem).attr('callback');
 		$("#"+click).on('click',function(){
-			ajaxPost(url,params,isLogin,callback);
+			checkLogin(url,params,isLogin,callback);
 		});
 		
-	}
+	};
 	
 	return customElem;
 });

b276f3c74948d060ad27d1ff17200aebc53b52d0
byqiqi <qifzl@qq.com>
2016-12-06 12:00:39 +0800

    Update mip-html-ajax.js

--- mip-html-ajax/mip-html-ajax.js
+++ mip-html-ajax/mip-html-ajax.js
@@ -7,11 +7,12 @@
 define(function (require) {
     var customElem = require('customElement').create();
     var qi = require('zepto');
-    var port = require('viewport');
-    var theid = 'Qping';
-    var errmsg = {
+    var viewPort = require('viewport');
+    var theID = 'Qping';
+    var errMsg = {
         r100: '获取配置json失败',
         r101: '配置json格式错误',
+        r102: '获取配置json.val错误',
         d100: '{name}格式不正确',
         g100: '提交请求出错',
         g101: '获取请求出错',
@@ -19,114 +20,112 @@ define(function (require) {
         g103: 'bad请求出错'
     };
 
-    function sethtmlajax(data, the, obj) {
+    function setHtmlAjax(data, the, obj) {
         if (typeof (data.set) === 'undefined') {
             return false;
         }
 		// 替换标签
         obj.find('val-input, val-textarea, val-select,val-option').each(function () {
-            var thishtml = qi(this).prop('outerHTML');
-            qi(this).prop('outerHTML', thishtml.replace(/<(\/?)val-/ig, '<$1'));
+            var thisHtml = qi(this).prop('outerHTML');
+            qi(this).prop('outerHTML', thisHtml.replace(/<(\/?)val-/ig, '<$1'));
         });
 		// 按钮事件
         obj.find(data.set.btn).click(function () {
-            gethtmlval(data, obj);
+            getHtmlVal(data, obj);
             return false;
         });
         // 回车事件
         the.addEventListener('keydown', function (event) {
             if (event.keyCode === 13) {
-                gethtmlval(data, obj);
+                getHtmlVal(data, obj);
                 return false;
             }
         }, false);
     }
 
 	// 获取检测提交数据
-    function gethtmlval(data, obj) {
-        var valarr = {};
+    function getHtmlVal(data, obj) {
+        if (typeof (data.val) === 'undefined') {
+            qAlert(errMsg.r102);
+            return false;
+        }
+        var valArr = {};
         for (var mycars in data.val) {
             if (data.val.hasOwnProperty(mycars)) {
-                var vdata = obj.find(data.val[mycars]);
-                var vvalalt = vdata.attr('val-alt') || '';
-                var vminlen = vdata.attr('min-len') || 0;
-                var vmaxlen = vdata.attr('max-len') || 0;
-                var vmsgstr = vvalalt !== '' ? vvalalt : errmsg.d100.replace('{name}', mycars);
-                if (vminlen === '*') {
-                    if (vdata.val().length === 0) {
-                        qalert(vmsgstr);
-                        return false;
-                    }
+                var vData = obj.find(data.val[mycars]);
+                var vValAlt = vData.attr('val-alt') || '';
+                var vMinLen = vData.attr('min-len') || 0;
+                var vMaxLen = vData.attr('max-len') || 0;
+                var vMsgStr = vValAlt !== '' ? vValAlt : errMsg.d100.replace('{name}', mycars);
+                if (vMinLen === '*' && vData.val().length === 0) {
+                    qAlert(vMsgStr);
+                    return false;
                 }
-                if (vminlen !== 0) {
-                    if (vdata.val().length < vminlen) {
-                        qalert(vmsgstr);
-                        return false;
-                    }
+                if (vMinLen !== 0 && vData.val().length < vMinLen) {
+                    qAlert(vMsgStr);
+                    return false;
                 }
-                if (vmaxlen !== 0) {
-                    if (vdata.val().length > vmaxlen) {
-                        qalert(vmsgstr);
-                        return false;
-                    }
+                if (vMaxLen !== 0 && vData.val().length > vMaxLen) {
+                    qAlert(vMsgStr);
+                    return false;
                 }
-                valarr[mycars] = vdata.val();
+                valArr[mycars] = vData.val();
             }
         }
-        gethtmlajax(data, obj, valarr);
+        getHtmlAjax(data, obj, valArr);
     }
 
 	// 提交数据
-    function gethtmlajax(data, obj, valarr) {
+    function getHtmlAjax(data, obj, valArr) {
         var getUrl = data.add.url;
-        var thebtn = obj.find(data.set.btn);
+        var theBtn = obj.find(data.set.btn);
         if (getUrl !== '') {
             getUrl = getUrl.replace('{sid}', data.id);
-            var gettype = ishttp(getUrl) ? 'jsonp' : 'json';
+            var getType = ishttp(getUrl) ? 'jsonp' : 'json';
             qi.ajax({
                 url: getUrl,
                 type: data.add.type,
-                dataType: gettype,
-                data: valarr,
+                dataType: getType,
+                data: valArr,
                 beforeSend: function () {
-                    thebtn.attr('disabled', 'disabled');
+                    theBtn.attr('disabled', 'disabled');
                     if (data.set.btnstr !== '') {
-                        thebtn.attr('btn-txt', thebtn.val()).val(data.set.btnstr);
+                        theBtn.attr('btn-txt', theBtn.val()).val(data.set.btnstr);
                     }
                 },
                 error: function () {
                     if (data.add.tip === '1') {
-                        qalert(errmsg.g100);
+                        qAlert(errMsg.g100);
                     }
-                    thebtn.removeAttr('disabled');
+                    theBtn.removeAttr('disabled');
                     if (data.set.btnstr !== '') {
-                        thebtn.val(thebtn.attr('btn-txt'));
+                        theBtn.val(theBtn.attr('btn-txt'));
                     }
                 },
                 success: function (jsondb) {
                     if (jsondb.success === 'err') {
-                        qalert(jsondb.msg);
+                        qAlert(jsondb.msg);
                     }
 					else {
                         var isretext = obj.find(data.res.upval).val();
                         obj.find('[empty=true]').val('');
                         if (jsondb.msg !== '') {
-                            qalert(jsondb.msg);
+                            qAlert(jsondb.msg);
                         }
                         if (isretext === '0') {
-                            var newdata = tempview(data.nta, jsondb);
-                            obj.find(data.obj.list).append(newdata);
+                            var newData = tempview(data.nta, jsondb);
+                            obj.find(data.obj.list).append(newData);
                         }
                         else {
-                            var adddata = tempview(data.ntr, jsondb);
-                            var addelem = obj.find(data.obj.list).find('#Q' + isretext);
-                            addelem.find(data.obj.listre).append(adddata);
-                            port.setScrollTop(addelem.offset().top);
+                            var addData = tempview(data.ntr, jsondb);
+                            var addElem = obj.find(data.obj.list).find('#Q' + isretext);
+                            addElem.find(data.obj.listre).append(addData);
+                            viewPort.setScrollTop(addElem.offset().top);
                         }
                     }
-                    thebtn.removeAttr('disabled');
+                    theBtn.removeAttr('disabled');
                     if (data.set.btnstr !== '') {
-                        thebtn.val(thebtn.attr('btn-txt'));
+                        theBtn.val(theBtn.attr('btn-txt'));
                     }
                 }
             });
@@ -134,18 +133,18 @@ define(function (require) {
     }
 
 	// 获取数据
-    function getcommentdata(data, obj, page) {
+    function getJsonData(data, obj, page) {
         var getUrl = data.get.url;
         if (getUrl !== '') {
             getUrl = getUrl.replace('{id}', data.id).replace('{page}', page);
-            var gettype = ishttp(getUrl) ? 'jsonp' : 'json';
+            var getType = ishttp(getUrl) ? 'jsonp' : 'json';
             qi.ajax({
                 url: getUrl,
                 type: data.get.type,
-                dataType: gettype,
+                dataType: getType,
                 error: function () {
                     if (data.get.tip === '1') {
-                        qalert(errmsg.g101);
+                        qAlert(errMsg.g101);
                     }
                     if (typeof (data.obj.nodata) !== 'undefined') {
                         if (data.obj.nodata === '1') {
@@ -155,12 +154,12 @@ define(function (require) {
                 },
                 success: function (jsondb) {
                     if (jsondb.state === 'err') {
-                        qalert(jsondb.msg);
+                        qAlert(jsondb.msg);
                     }
                     else {
                         if (typeof (jsondb[data.obj.arr]) !== 'undefined') {
                             if (jsondb[data.obj.arr].length > 0) {
-                                var datalist = '';
+                                var dataList = '';
                                 for (var i = 0; i < jsondb[data.obj.arr].length; i++) {
                                     var thisdata = tempview(data.hta, jsondb[data.obj.arr][i]);
                                     if (thisdata.indexOf('$ReData$') > 0 && data.obj.rearr !== '') {
@@ -170,9 +169,9 @@ define(function (require) {
                                         }
                                         thisdata = thisdata.replace('$ReData$', redata);
                                     }
-                                    datalist += thisdata;
+                                    dataList += thisdata;
                                 }
-                                obj.find(data.obj.list).append(datalist);
+                                obj.find(data.obj.list).append(dataList);
                             }
 							else {
                                 if (typeof (data.obj.nodata) !== 'undefined') {
@@ -194,7 +193,7 @@ define(function (require) {
                                     iMore.html(iText);
                                 }
                                 iMore.show();
-                                iMore.attr('on', 'tap:' + theid + '.Qmore(' + (iPage + 1) + ')');
+                                iMore.attr('on', 'tap:' + theID + '.Qmore(' + (iPage + 1) + ')');
                             }
                             else {
                                 iMore.remove();
@@ -210,45 +209,47 @@ define(function (require) {
     }
 
     // on点击事件
-    function sethtmlclick(data, object, obj) {
+    function setHtmlClick(data, object, obj) {
         object.addEventAction('Qres', function (event, str) {
             obj.find(data.res.show).show().find(data.res.upint).text(str);
-            port.setScrollTop(obj.find(data.set.txt).offset().top);
-            setTimeout(function () {
+            viewPort.setScrollTop(obj.find(data.set.txt).offset().top);
+            var QresOut = setTimeout(function () {
                 obj.find(data.set.txt).focus();
+                clearTimeout(QresOut);
             }, 200);
         });
         object.addEventAction('Qreh', function (event, str) {
             obj.find(data.res.show).hide().find('em').text(0);
-            setTimeout(function () {
+            var QrehOut = setTimeout(function () {
                 obj.find(data.set.txt).blur();
+                clearTimeout(QrehOut);
             }, 200);
         });
         object.addEventAction('Qrev', function (event, str) {
             obj.find(data.res.upval).val(str);
         });
         object.addEventAction('Qmore', function (event, str) {
-            var themore = obj.find(data.obj.more);
-            themore.attr('more-txt', themore.html()).html(data.obj.morestr);
-            getcommentdata(data, obj, str);
+            var theMore = obj.find(data.obj.more);
+            theMore.attr('more-txt', theMore.html()).html(data.obj.morestr);
+            getJsonData(data, obj, str);
         });
         object.addEventAction('Qdig', function (event, str) {
             var getUrl = data.dig.url;
             if (getUrl !== '') {
                 getUrl = getUrl.replace('{sid}', str);
-                var gettype = ishttp(getUrl) ? 'jsonp' : 'json';
+                var getType = ishttp(getUrl) ? 'jsonp' : 'json';
                 qi.ajax({
                     url: getUrl,
                     type: data.dig.type,
-                    dataType: gettype,
+                    dataType: getType,
                     error: function () {
                         if (data.dig.tip === '1') {
-                            qalert(errmsg.g102);
+                            qAlert(errMsg.g102);
                         }
                     },
                     success: function (jsondb) {
                         if (jsondb.success === 'err') {
-                            qalert(jsondb.msg);
+                            qAlert(jsondb.msg);
                         }
 						else {
                             qi(event.target).prepend('<i class="Qdig">+1</i>');
@@ -266,19 +267,19 @@ define(function (require) {
             var getUrl = data.bad.url;
             if (getUrl !== '') {
                 getUrl = getUrl.replace('{sid}', str);
-                var gettype = ishttp(getUrl) ? 'jsonp' : 'json';
+                var getType = ishttp(getUrl) ? 'jsonp' : 'json';
                 qi.ajax({
                     url: getUrl,
                     type: data.bad.type,
-                    dataType: gettype,
+                    dataType: getType,
                     error: function () {
                         if (data.bad.tip === '1') {
-                            qalert(errmsg.g103);
+                            qAlert(errMsg.g103);
                         }
                     },
                     success: function (jsondb) {
                         if (jsondb.success === 'err') {
-                            qalert(jsondb.msg);
+                            qAlert(jsondb.msg);
                         }
 						else {
                             qi(event.target).prepend('<i class="Qbad">+1</i>');
@@ -296,7 +297,7 @@ define(function (require) {
 
     // 替换模板标签
     function tempview(str, arr) {
-        str = str.replace(/{thisid}/gi, theid);
+        str = str.replace(/{thisid}/gi, theID);
         var sArr = str.match(/{\w+}/gi);
         if (sArr) {
             for (var i = 0; i < sArr.length; i++) {
@@ -308,58 +309,59 @@ define(function (require) {
     }
 
     // 弹出提示层
-    function qalert(msg, s) {
-        s = s || 1600;
-        if (qi('#Qmidalert').length > 0) {
-            qi('#Qmidalert').remove();
+    function qAlert(msg, ms) {
+        ms = ms || 1600;
+        if (qi('.mip-html-ajax-tip').length > 0) {
+            qi('.mip-html-ajax-tip').remove();
         }
-        qi('body').append('<div id="Qmidalert">' + msg + '</div>');
-        setTimeout(function () {
-            qi('#Qmidalert').fadeOut(100, function () {
+        qi('body').append('<div class="mip-html-ajax-tip">' + msg + '</div>');
+        var msgOut = setTimeout(function () {
+            qi('.mip-html-ajax-tip').fadeOut(100, function () {
                 qi(this).remove();
             });
-        }, s);
+            clearTimeout(msgOut);
+        }, ms);
     }
 
-    // 验证是否http
+    // 验证是否http开头
     function ishttp(str) {
-        var orreg = false;
+        var orReg = false;
         var isreg = /^(http|https):\/\//i;
         if (isreg.exec(str)) {
-            orreg = true;
+            orReg = true;
         }
 		else {
-            orreg = false;
+            orReg = false;
         }
-        return orreg;
+        return orReg;
     }
 
     // build 方法，元素插入到文档时执行，仅会执行一次
     customElem.prototype.build = function () {
-        var thisobj = this.element;
-        var element = qi(thisobj);
-        var elemobj = element.find('script[type$=json]');
-        var elemids = element.attr('id');
-        if (elemids) {
-            theid = elemids;
+        var thisObj = this.element;
+        var theElem = qi(thisObj);
+        var elemObj = theElem.find('script[type$=json]');
+        var elemIds = theElem.attr('id');
+        if (elemIds) {
+            theID = elemIds;
         }
 		else {
-            element.attr('id', theid);
+            theElem.attr('id', theID);
         }
-        if (elemobj.length === 0) {
-            element.html(errmsg.r100);
+        if (elemObj.length === 0) {
+            theElem.html(errMsg.r100);
             return false;
         }
         try {
-            var data = JSON.parse(elemobj.text());
+            var data = JSON.parse(elemObj.text());
         }
         catch (e) {
-            element.html(errmsg.r101);
+            theElem.html(errMsg.r101);
             return false;
         }
-        getcommentdata(data, element, 1);
-        sethtmlajax(data, thisobj, element);
-        sethtmlclick(data, this, element);
+        getJsonData(data, theElem, 1);
+        setHtmlAjax(data, thisObj, theElem);
+        setHtmlClick(data, this, theElem);
     };
     return customElem;
 });

2d4c2c73ceab9d6e56692505d63efd758a6ab2d9
hejieye <ishejieye@126.com>
2016-12-08 15:23:12 +0800

    Update mip-iask-report.js

--- mip-iask-report/mip-iask-report.js
+++ mip-iask-report/mip-iask-report.js
@@ -8,20 +8,20 @@ define(function (require) {
     var $ = require('zepto');
     var customElem = require('customElement').create();
     var openReportDiv = function (elem) {
-        $('#report-div').show();
+        $('.report-body').show();
         var questionId = $(elem).attr('questionId');
         var type = $(elem).attr('type');
         var typeId = $(elem).attr('typeId');
-        $('#report_id').text(questionId);
-        $('#report_type').text(type);
-        $('#report_typeId').text(typeId);
+        $('.report_id').text(questionId);
+        $('.report_type').text(type);
+        $('.report_typeId').text(typeId);
     };
     // build 方法，元素插入到文档时执行，仅会执行一次
     customElem.prototype.build = function () {
         var elem = this.element;
         var click = $(elem).attr('typeId');
         var type = $(elem).attr('type');
-        $('#' + type + '_' + click).on('click',
+        $('.' + type + '_' + click).on('click',
         function () {
             // 打开举报div
             openReportDiv(elem);

74728d93f1d2798f433ff905eaa8771fc28ffaff
huanghuixin <274919800@qq.com>
2016-12-09 10:49:34 +0800

    去掉广告倒计时时间提示,修改视频切换的逻辑:改为onended事件触发

--- mip-ad-video/mip-ad-video.js
+++ mip-ad-video/mip-ad-video.js
@@ -18,14 +18,10 @@ define(function (require) {
         var $element = $(this.element);
         var adSrc = $element.attr('ad-src');
         var targetSrc = $element.attr('target-src');
-        var adTime = $element.attr('ad-time');
         var poster = $element.attr('poster');
 
         //  初始化播放器
         var video = document.createElement('video');
-        //  广告剩余时间提醒
-        var spanAdTime = document.createElement('span');
-        spanAdTime.innerHTML = adTime + '';
 
         //  初始化video的属性
         $(video).attr({
@@ -37,38 +33,24 @@ define(function (require) {
 
         //  如果是IOS则设置为自动播放
         if (platform.isIos()) {
-            $(video).attr({
-                'autoPlay': ''
-            });
+            video.autoplay = true;
         }
 
         $element[0].appendChild(video);
 
         //  如果有广告并且非IOS上的QQ浏览器 则播放广告
         if (adSrc && !(platform.isIos() && platform.isQQ())) {
-            $element[0].appendChild(spanAdTime);
             video.src = adSrc;
 
-            //  当开始播放 或者点击播放的时候 进行广告倒计时
-            video.onplay = video.onclick = function () {
-                video.onplay = video.onclick = null;
-                var timeId = setInterval(function () {
-                    adTime--;
-                    //  刷新显示的广告秒
-                    spanAdTime.innerHTML = adTime;
-                    //  广告播放完毕
-                    if (adTime <= 0) {
-                        spanAdTime.style.display = 'none';
-                        clearInterval(timeId);
-                        video.setAttribute('src', targetSrc);
-                        video.autoplay = true;
-                        // video.reload();
-                    }
-                }, 1000);
+            //  广告播放完毕
+            video.onended = function () {
+                video.src =  targetSrc;
+                video.autoplay = true;
+                video.load();
+                video.play();
             };
         } else {  //  否则直接播放内容
             video.src = targetSrc;
-            video.setAttribute('controls', '');
         }
     };
 

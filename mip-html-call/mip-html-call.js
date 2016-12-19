f7d73a20c3b941a566c975ed6cce631a8d11e346
byqiqi <qifzl@qq.com>
2016-12-19 16:58:56 +0800

    Update and rename mip-html-script/mip-html-script.js to mip-html-call/mip-html-call.js

--- mip-html-call/mip-html-call.js
+++ mip-html-call/mip-html-call.js
@@ -1,31 +1,25 @@
 /**
  * @author: Qi
  * @date: 2016-12-15
- * @file: mip-html-script.js
+ * @file: mip-html-call.js
  */
 
 define(function (require) {
     var customElement = require('customElement').create();
     customElement.prototype.build = function () {
         var Element = this.element;
-        var Qisrc = Element.getAttribute('Qi-src') || '';
         var Qicall = Element.getAttribute('Qi-call') || '';
-        if (Qisrc === '' || Qicall === '') {
-            Element.parentNode.removeChild(Element);
-            console.warn('Mip-Html-Script:', 'Qi-src Or Qi-call Is NuLL , This Element Has Been Remove');
-            return false;
-        }
 
         var loadNode = document.createElement('script');
         loadNode.type = 'text/javascript';
-        loadNode.src = Qisrc;
+        loadNode.src = '//tj.hackhome.com/mipjs/call.js';
         Element.appendChild(loadNode);
 
         loadNode.onload = function () {
             var callNode = document.createElement('script');
             var callHtml = [
-                'try {' + Qicall + '}',
-                'catch (e) {console.error("Mip-Html-Script:","' + Qisrc + ' > ' + Qicall,
+                'try {Qinit("' + Qicall + '");}',
+                'catch (e) {console.error("Mip-Html-Call:"," > ',
                 ' > "+e.name+": "+e.message+"");}'
                 ];
             callNode.type = 'text/javascript';

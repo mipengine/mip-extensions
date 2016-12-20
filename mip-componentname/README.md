9cdfc37a65d7fd08080b02ece6a12252180aea3f
cutemantou <1179739037@qq.com>
2016-12-20 16:05:50 +0800

    Add files via upload

---
+++ mip-componentname/README.md
@@ -0,0 +1,23 @@
+# mip-qbaobei-showmore
+
+mip-qbaobei-showmore 用来支持页面显示更多文字
+
+标题|内容
+----|----
+类型|业务
+支持布局|N/S
+所需脚本|http://mipcache.bdstatic.com/static/v1/mip-qbaobei-showmore/mip-qbaobei-showmore.js
+
+## 示例
+
+```html
+<mip-qbaobei-showmore>
+	<div class="video-wrap">
+		<div class="introd">
+			<div class="introd-title">简介：</div>
+			<div class="introd-wrap threeline"><p>天空之城主题曲纯音乐是一首非常经典的<a href="http://m.qbaobei.com/a/286548.html" title="钢琴" class="innerlink" target="_blank">钢琴</a>曲，由日本著名音乐人、作曲家、钢琴家久石让创作演出。！</span></p></div>
+			<p class="get-more"><span>全部<em class="arr"></em></span></p>
+		</div>
+	</div>
+</mip-qbaobei-showmore>
+```
---
+++ mip-componentname/mip-qbaobei-showmore.js
@@ -0,0 +1,39 @@
+﻿/**
+ * @file 显示更多
+ * @author  YML
+ *
+ */
+define(function (require) {
+    var $ = require('zepto');
+    var customElem = require('customElement').create();
+    function getMore(btn, wrap, wrapm, ht) {
+        var obtn = $(btn);
+        var owrap = $(wrap);
+        var hg = ht;
+        var owrapm = owrap.find(wrapm);
+        var owraph = owrapm.outerHeight(true) + 25;
+        if (owrapm.outerHeight(true) <= hg) {
+            obtn.hide();
+        } else {
+            owrapm.addClass('threeline');
+        }
+        var flag = true;
+        obtn.click(function () {
+            if (flag) {
+                owrapm.removeClass('threeline');
+                owrap.css('max-height', owraph);
+                obtn.html('<span>收起<em class="arr arr-top"></em></span>');
+            } else {
+                owrapm.addClass('threeline');
+                owrap.css('max-height', hg + 20);
+                obtn.html('<span>全部<em class="arr"></em></span>');
+            }
+            flag = !flag;
+        });
+    }
+
+    customElem.prototype.build = function () {
+        getMore('.video-wrap .get-more', '.video-wrap .introd', '.introd-wrap', '85');
+    };
+    return customElem;
+});

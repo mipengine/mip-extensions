2fe8725c47ea1addc973cf2fd1bd7a35d9d2614d
dinglei123 <weiyoubaba@vip.163.com>
2016-12-23 14:00:30 +0800

    已修改
    麻烦检查下。

--- mip-weiyoubaba-tthead/mip-weiyoubaba-tthead.js
+++ mip-weiyoubaba-tthead/mip-weiyoubaba-tthead.js
@@ -1,52 +1,46 @@
 
 /**
- * 微友巴巴mip改造 javascript功能插件
- * @file 导航条单击展开更多分类
- * @date 2016.12.12
- * @author dinglei (375234944@qq.com)
- * @version 1.0.1
- */
+* 微友巴巴mip改造 javascript功能插件
+* @file 导航条单击展开更多分类
+* @date 2016.12.12
+* @author dinglei (375234944@qq.com)
+* @version 1.0.1
+*/
 
 define(function (require) {
 
-var $ = require('zepto');
-var customElem = require('customElement').create();
+    var $ = require('zepto');
+    var customElem = require('customElement').create();
 
-function Open()
-{
-	$(".icon_open").on("click",function(){
-        $(".nav .hide").removeClass("hide");
-        $(".icon_open").hide();
-        $(".icon_close").show();
-        $(".icon_close").css("display","block");
+    function open() {
+        $('.icon_open').on('click', function () {
+            $('.nav .hide').removeClass('hide');
+            $('.icon_open').hide();
+            $('.icon_close').show();
+            $('.icon_close').css('display', 'block');
 
-        var p_top = $(".nav").height() + $(".w98").height()+1;
-        $(".container").css("padding-top", p_top + "px");
-    });
+            var top = $('.nav').height() + $('.w98').height() + 1;
+            $('.container').css('padding-top', top + 'px');
+        });
+    }
 
-}
+    function close() {
+        $('.icon_close').on('click', function () {
+            $('.open').addClass('hide');
+            $('.icon_open').show();
+            $('.icon_close').hide();
 
-function Close()
-{
-   $(".icon_close").on("click",function(){
-        $(".open").addClass("hide");
-        $(".icon_open").show();
-        $(".icon_close").hide();
+            var top = $('.nav').height() + $('.w98').height() + 1;
+            $('.container').css('padding-top', top + 'px');
+        });
+    }
 
-        var p_top = $(".nav").height() + $(".w98").height()+1;
-        $(".container").css("padding-top", p_top + "px");
-    });
-}
+    function init() {
+        open();
+        close();
+    }
 
-function Init()
-{
-	Open();
-	Close();
-}
+    customElem.prototype.build = init;
 
- customElem.prototype.build = function () {
-        Init();
-    };
     return customElem;
-
 });

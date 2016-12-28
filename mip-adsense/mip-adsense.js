d8c187ef504066485d17cd6e08c02884cb657b61
smileU <1484249625@qq.com>
2016-12-28 17:28:51 +0800

    Update mip-adsense.js

--- mip-adsense/mip-adsense.js
+++ mip-adsense/mip-adsense.js
@@ -11,6 +11,7 @@ define(function (require) {
         var className = ele.getAttribute('class');
         var dataAdClient = ele.getAttribute('data-ad-client');
         var dataAdSlot = ele.getAttribute('data-ad-slot');
+        var dataAdFormat = ele.getAttribute('data-ad-format');
         var htmlCon = document.getElementsByTagName('head')[0] || document.head || document.documentElement;
         var tag1 = document.createElement('script');
         tag1.setAttribute('type', 'text/javascript');
@@ -21,6 +22,9 @@ define(function (require) {
         tag2.setAttribute('class', className);
         tag2.setAttribute('data-ad-client', dataAdClient);
         tag2.setAttribute('data-ad-slot', dataAdSlot);
+        tag2.setAttribute('data-ad-format', dataAdFormat);
+        tag2.setAttribute('class', 'adsbygoogle');
+        tag2.setAttribute('style', 'display:block');
         htmlCon.appendChild(tag2);
         var tag3 = document.createElement('script');
         tag3.innerHTML('(adsbygoogle=window.adsbygoogle||[]).push({});');

6bb5f895a6380763b65f1cf06de7a42d9a1b67c7
jfdsies <jfdsies@gmail.com>
2017-01-04 13:37:28 +0800

    FECS: Everything is OK!
    createdCallback 改了
    fecs这规范真™规范，如果公司采用这个，我肯定被前端揍了。。。

--- mip-content-readmore/mip-content-readmore.js
+++ mip-content-readmore/mip-content-readmore.js
@@ -1,39 +1,41 @@
 /**
-* @file 查看更多文本内容
-* @author jfdsies@gmail.com
-* @version 1.0.0
-* @copyright 2016 yjbys.com, Inc. All Rights Reserved
-*/
+ * @file 查看更多文本内容
+ * @author jfdsies@gmail.com
+ * @version 1.0.0
+ * @copyright 2016 yjbys.com, Inc. All Rights Reserved
+ */
 
 define(function (require) {
-    var $ = function(selector) {
+    var $ = function (selector) {
         var selectorType = 'querySelectorAll';
 
-        if(selector.indexOf('#') === 0){
+        if (selector.indexOf('#') === 0) {
             selectorType = 'getElementById';
             selector = selector.substr(1, selector.length);
-        }else if(selector.indexOf('.') === 0){
+        }
+        else if (selector.indexOf('.') === 0) {
             selectorType = 'getElementsByClassName';
             selector = selector.substr(1, selector.length);
         }
-
         return document[selectorType](selector);
     };
     var customElement = require('customElement').create();
 
-    function html_decode(str) {
-        var s = "";
-        if (str.length < 1) return "";
-        s = str.replace(/&/g, "&");
-        s = s.replace(/</g, "<");
-        s = s.replace(/>/g, ">");
-        s = s.replace(/ /g, " ");
-        s = s.replace(/'/g, "\'");
-        s = s.replace(/"/g, "\"");
+    function htmlDecode(str) {
+        var s = '';
+        if (str.length < 1) {
+            return '';
+        }
+        s = str.replace(/&/g, '&');
+        s = s.replace(/</g, '<');
+        s = s.replace(/>/g, '>');
+        s = s.replace(/ /g, ' ');
+        s = s.replace(/'/g, '\'');
+        s = s.replace(/"/g, '\"');
         return s;
     }
 
-    customElement.prototype.build = function () {
+    customElement.prototype.createdCallback = function () {
         var me = this.element;
         var maxlen = me.getAttribute('maxlen');
         var buttitle = me.getAttribute('buttitle');
@@ -42,33 +44,40 @@ define(function (require) {
         buttitle = buttitle ? buttitle : '查看更多';
         element = element ? $(element)[0] : $('.content')[0];
 
-        var content = html_decode(element.innerHTML);
+        var content = htmlDecode(element.innerHTML);
         var conlen = content.length;
-        var intag = false, len = 0, tmp = '';
-        for(var i = 0;;i++){
-            if(len >= maxlen){
-                len = i;break;
+        var intag = false;
+        var len = 0;
+        var tmp;
+        for (var i = 0; ; i++) {
+            if (len >= maxlen) {
+                len = i;
+                break;
             }
-            if(i >= conlen){
-                len = conlen;break;
+            if (i >= conlen) {
+                len = conlen;
+                break;
             }
             tmp = content.charAt(i);
-            if(tmp == '<' && !intag){
+            if (tmp === '<' && !intag) {
                 intag = true;
                 continue;
-            }else if(tmp == '>' && intag){
+            }
+            else if ('>' === tmp && intag) {
                 intag = false;
                 continue;
-            }else if(intag || tmp=='' || tmp==' ' || tmp=='\r' || tmp=='\n'){
+            }
+            else if (intag || tmp === '' || tmp === ' ' || tmp === '\r' || tmp === '\n') {
                 continue;
             }
             len++;
         }
-        if(content != undefined && conlen > len){
-            me.innerHTML = '<div class="mip-content-readmore"><em><code class="mip-readmore-fx"></code></em><span>'+buttitle+'</span></div>';
-            element.innerHTML = content.substring(0, len) + " ......";
+        if (content !== undefined && conlen > len) {
+            me.innerHTML = '<div class="mip-content-readmore"><em><code class="mip-readmore-fx"></code></em><span>'
+                + buttitle + '</span></div>';
+            element.innerHTML = content.substring(0, len) + ' ......';
             var readmoreele = $('.mip-content-readmore');
-            readmoreele[readmoreele.length-1].addEventListener('click', function(){
+            readmoreele[readmoreele.length - 1].addEventListener('click', function () {
                 element.innerHTML = content;
                 me.innerHTML = '';
             }, false);

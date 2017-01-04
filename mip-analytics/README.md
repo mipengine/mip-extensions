c00744712bdfffa2dea7c6a8ba9870e413016f52
Alan110 <306880673@qq.com>
2017-01-04 16:25:15 +0800

    mip log interface

---
+++ mip-analytics/README.md
@@ -0,0 +1,153 @@
+# mip-analytics
+
+mip-analytics 提供统计发送接口，由使用方决定在什么时候发送什么参数，到什么地方。
+
+标题|内容
+----|----
+类型|通用
+支持事件|click,touchend,disp,scroll,timer
+所需脚本|http://mipcache.bdstatic.com/static/mip-analytics/{版本号}/mip-analytics.js
+
+## 示例
+
+### 基本用法
+
+```html
+<div class="test" style="height:200px; background-color:blue;"></div>
+<div class="test2" style="height:200px; background-color:yellow;"></div>
+
+
+<mip-analytics>
+<script type="application/json">
+{
+    "hosts" : {
+		"mylogserver" : "https://m.baidu.com/log?",
+		"mylogserver2" : "https://m.baidu.com/test?event=click&"
+	},
+
+    "setting" : {
+
+        "click" : [
+            {
+				"selector" : ".test",
+                "host" : "mylogserver",
+                "data" : {
+					"name" : "alan",
+					"list": {
+						"age":"123"
+					}
+				}
+            },
+
+            {
+				"selector" : ".test2",
+                "host" : "mylogserver",
+                "data" : {
+					"name" : "alan",
+					"list": {
+						"age":"123fdafdsfadfafdfdfda"
+					}
+				
+				}
+            }
+        ],
+
+        "disp" : [],
+        "timer" : [
+			{
+				"host" : "mylogserver2",
+				"data" : {
+					"timer" : "timer"
+				},
+				"option" : {
+					"interval" : 2000
+				}
+			}
+		],
+        "scroll" : []
+    }
+}
+</script>
+</mip-analytics>
+
+```
+
+## 配置参数
+
+### {hosts}
+
+说明：{指定用到的log server 地址, 用以后面配置复用}
+必选项：{是}
+类型：{键值对}
+取值范围：{https}
+
+### {setting}
+
+说明：{配置日志发送}
+必选项：{是}
+类型：{键值对}
+
+___
+
+#### {setting.click}
+
+说明：{配置点击事件}
+必选项：{否}
+类型：{键值对}
+
+##### {setting.click.selector}
+
+说明：{指定触发点击的选择器}
+必选项：{是}
+类型：{css选择器}
+
+##### {setting.click.host}
+
+说明：{指定日志发送的log server}
+必选项：{是}
+类型：{hosts参数中的key}
+
+##### {setting.click.data}
+
+说明：{指定日志的querystring}
+必选项：{否}
+类型：{键值对}
+
+___
+
+#### {setting.touchstart}
+
+说明：{完全同click}
+
+___
+
+#### {setting.timer}
+
+说明：{定时发送日志设置}
+必选项：{否}
+类型：{键值对}
+
+#### {setting.timer.host}
+
+说明：{同click.host}
+
+#### {setting.timer.data}
+
+说明：{同click.data}
+
+#### {setting.timer.option.interval}
+
+说明：{指定定时器间隔}
+必选项：{否}
+类型：{数字}
+单位：{ms}
+默认值：{4000ms}
+
+## 注意事项
+
+* 注意是标准json, 属性名，属性值都要用双引号
+
+* 统计框架会默认传入一级参数t, 为发送日志的时间
+
+* host地址需要是https的
+
---
+++ mip-analytics/mip-analytics.js
@@ -0,0 +1,147 @@
+/**
+ * @file mip-analytics 组件
+ * @author lijialong  306880673@qq.com
+ */
+
+define(function (require) {
+
+    var customElement = require('customElement').create();
+    var util =  require('util');
+
+    /**
+     * 构造元素，只会运行一次
+     */
+    customElement.prototype.build = function () {
+
+	// 获取config
+	try{
+	    var script = this.element.querySelector('script[type="application/json"]');
+	    var cfg = this.cfg = JSON.parse(script.textContent.toString());
+	}catch(e){
+	    console.warn("json is illegal");
+	    console.warn(e);
+	    return;
+	}
+
+	// 全局代理事件
+	for (var prop in this.cfg.setting){
+	    // 替换host变量
+	    var events = cfg.setting[prop];
+	    events.forEach(function(el,index){
+		el.host = cfg.hosts[el.host];
+	    });
+	    triggers[prop].call(this,events);
+	}
+    };
+
+    var log = {
+
+        isObject: function (obj) {
+            return typeof obj === 'object';
+        },
+
+        /**
+         * 数据序列化处理
+         *
+         * @param obj
+         * @returns {string}
+         */
+        serialize: function (obj) {
+            if (!obj) {
+                return '';
+            }
+
+            var str = '';
+            var item = '';
+            if (this.isObject(obj)) {
+                for (var k in obj) {
+                    item = obj[k];
+                    if (typeof item === 'undefined') {
+                        continue;
+                    }
+
+                    if (this.isObject(item)) {
+                        item = JSON.stringify(item);
+                    }
+
+                    str += k + '=' + encodeURIComponent(item) + '&';
+                }
+                str = str.substring(0, str.length - 1); // 去掉末尾的&
+            }
+            else if (this.isString(obj)) {
+                str = obj;
+            }
+
+            return str;
+        },
+
+        /**
+         * 使用img的方式发送日志
+         *
+         * @param url src链接
+         * @returns {undefined}
+         */
+        imgSendLog: function (url) {
+            var key = 'BD_MIP' + (new Date()).getTime();
+            var img = window[key] = new Image();
+            img.onload = function () {
+                // 防止多次触发onload;
+                img.onload = img.onerror = img.onabort = null;
+                // 清空引用,避免内存泄漏
+                window[key] = null;
+                img = null;
+            };
+            img.src = url;
+        },
+
+	send : function(cfg){
+            var queryString = this.serialize(cfg.data) + '&t=' + new Date().getTime();
+	    var url = cfg.host + queryString;
+	    this.imgSendLog(url);
+	}
+    };
+
+
+    // 点击事件handle
+    var clickHandle = function(triggers, eventName){
+	document.body.addEventListener(eventName, function(event){
+	    triggers.forEach(function(el,index){
+		if (util.dom.matches(event.target,el.selector)) {
+		    log.send(el);
+		}
+	    });
+	},false);
+    };
+
+    // 定时器存储
+    var timer = [];
+
+    // event bind
+    var triggers = {
+	click : function(triggers){
+	    clickHandle(triggers,"click");
+	},
+	touchend : function(triggers){
+	    clickHandle(triggers,"touchend");
+	},
+	disp : function(triggers){
+	},
+	scroll : function(){},
+	timer : function(triggers){
+	    triggers.forEach(function(el,index){
+		timer.push(setInterval(function(){
+		    log.send(el);
+		},el.option.interval || 4000));
+	    });
+	}
+    };
+
+    // 销毁工作
+    customElement.prototype.detachedCallback = function () {
+	timer.forEach(function(el,index){
+	    clearInterval(el);
+	});
+    };
+
+    return customElement;
+});

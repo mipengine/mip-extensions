9aacd959a13af3adea8639a966236680d4ea83fe
smileU <1484249625@qq.com>
2017-01-09 10:29:07 +0800

    Update mip-changyan.js

--- mip-changyan/mip-changyan.js
+++ mip-changyan/mip-changyan.js
@@ -1,8 +1,8 @@
 /**
  * @file
-* 畅言插件
-* @author smileU
-* @version 1.0.0
+ * 畅言插件
+ * @author smileU
+ * @version 1.0.1
 */
 define(function (require) {
     var customElement = require('customElement').create();
@@ -13,39 +13,15 @@ define(function (require) {
         var criWidth = ele.getAttribute('critical-width');
         var viewport = require('viewport');
         var width = viewport.getWidth();
-        if (criWidth != null && width < criWidth) {
-            var html = '<script id="changyan_mobile_js" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>';
-            window.document.write(html);
+        var scriptTag = document.createElement('script');
+        scriptTag.setAttribute('type', 'text/javascript');
+        scriptTag.setAttribute('charset', 'UTF-8');
+        if (width < criWidth) {
+            scriptTag.id = 'changyan_mobile_js';
+            scriptTag.setAttribute('src', 'http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf);
         }
         else {
-            var loadJs = function (srcStr, fn) {
-                var htmlTag = document.getElementsByTagName('head')[0] || document.head || document.documentElement;
-                var scriptTag = document.createElement('script');
-                scriptTag.setAttribute('type', 'text/javascript');
-                scriptTag.setAttribute('charset', 'UTF-8');
-                scriptTag.setAttribute('src', srcStr);
-                if (typeof fn === 'function') {
-                    if (window.attachEvent) {
-                        scriptTag.onreadystatechange = function () {
-                            var e = scriptTag.readyState;
-                            if (e === 'loaded' || e === 'complete') {
-                                scriptTag.onreadystatechange = null;
-                                fn();
-                            }
-                        };
-                    }
-                    else {
-                        scriptTag.onload = fn;
-                    }
-                }
-                htmlTag.appendChild(scriptTag);
-            };
-            loadJs('https://changyan.sohu.com/upload/changyan.js', function () {
-                window.changyan.api.config({
-                    appid: appid,
-                    conf: conf
-                });
-            });
+            scriptTag.src = 'http://changyan.sohu.com/upload/changyan.js?client_id=' + appid + '&conf=' + conf;
         }
     };
     return customElement;

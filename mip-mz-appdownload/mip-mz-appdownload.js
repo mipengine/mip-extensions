78b2d3456d74d63cb9936881d10c39c557eede1e
pifire <104460712@qq.com>
2017-01-10 15:56:48 +0800

    改cookie

--- mip-mz-appdownload/mip-mz-appdownload.js
+++ mip-mz-appdownload/mip-mz-appdownload.js
@@ -24,7 +24,7 @@ define(function (require) {
                 $('.down').css('height', '90px');
                 var presenti = 0;
                 for (var i = 0; i < obj.android.length; ++i) {
-                    if (!getCookie(obj.android[i].id)) {
+                    if (!localStorage.getItem(obj.android[i].id)) {
                         presenti = i;
                         break;
                     }
@@ -49,7 +49,7 @@ define(function (require) {
         var gsdbtn = document.getElementById('gsdbtn');
         var presenti = 0;
         for (var i = 0; i < androidAD.length; ++i) {
-            if (!getCookie(androidAD[i].id)) {
+            if (!localStorage.getItem(androidAD[i].id)) {
                 presenti = i;
                 break;
             }
@@ -67,24 +67,6 @@ define(function (require) {
             gsdbtn.setAttribute('href', checkurl(aid, addr));
             gsdbtn.innerText = androidAD[presenti].ubtnvalue;
             yybtext.innerText = androidAD[presenti].uinfo;
-        }
-    }
-	// 设置cookie
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = 'expires=' + d.toGMTString();
-        document.cookie = cname + '=' + cvalue + ';expires=' + expires;
-    }
-    // 获取cookie
-    function getCookie(cname) {
-        var name = cname + '=';
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i].trim();
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length);
-            }
         }
     }
 	// 数组中是否包含
@@ -120,10 +102,10 @@ define(function (require) {
                     for (var i = 0; i < json.android.length; ++i) {
                         var j = (i === json.android.length - 1) ? 0 : i + 1;
                         if ($('.gsdbtn').attr('presentid') === json.android[i].id) {
-                            setCookie(json.android[j].id, '', 1);
+                            localStorage.removeItem(json.android[j].id);
                         }
                         else {
-                            setCookie(json.android[j].id, 1, 1);
+                            localStorage.setItem(json.android[j].id, 1);
                         }
                     }
                 }

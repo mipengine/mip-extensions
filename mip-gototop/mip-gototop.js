b8587710c246f142eeae4c1b3fc7f1ca46796153
452242153@qq.com <452242153@qq.com>
2017-01-17 10:57:43 +0800

    change const to var

--- mip-gototop/mip-gototop.js
+++ mip-gototop/mip-gototop.js
@@ -9,8 +9,7 @@ define(function (require) {
     var customElement = require('customElement').create();
     var util = require('util');
     var viewport = require('viewport');
-    const YOFFSET = 200;
-    const DELAY = 0;
+    var yOffset = 200;
 
     /**
      * build 组件build
@@ -18,8 +17,8 @@ define(function (require) {
     customElement.prototype.build = function () {
         var self = this;
         var element = self.element;
-        var threshold = element.getAttribute('threshold') || YOFFSET;
-        var delay = parseInt(element.getAttribute('delay'), 10) || DELAY;
+        var threshold = element.getAttribute('threshold') || yOffset;
+        var delay = parseInt(element.getAttribute('delay'), 10) || 0;
         var timmer;
 
         viewport.on('scroll', function () {

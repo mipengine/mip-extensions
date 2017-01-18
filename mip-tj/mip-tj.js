890db3820ec1be953716736a4fec828702c8bac4
pengping <316476361@qq.com>
2017-01-18 18:09:34 +0800

    first commit

---
+++ mip-tj/mip-tj.js
@@ -0,0 +1,13 @@
+/**
+ * @file 移动适配图片的相关推荐
+ * @date 2017.01.18
+ * @author pp
+ * @copyright 2017 tuotian.com, Inc. All Rights Reserved
+ */
+
+
+define(function (require) {
+    var node = document.createElement('script');
+    node.setAttribute('src', 'https://sdgg.gucheng.com/s.html?id=55');
+    document.body.appendChild(node);
+});

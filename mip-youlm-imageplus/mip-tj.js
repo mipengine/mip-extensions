76c17a76eef7938ff26f8932677ea423b0345ba4
pengping <316476361@qq.com>
2017-01-19 11:11:22 +0800

    first commit

---
+++ mip-youlm-imageplus/mip-tj.js
@@ -0,0 +1,13 @@
+/**
+ * @file 移动图加广告的相关推荐
+ * @date 2017.01.18
+ * @author pp
+ * @copyright 2017 tuotian.com, Inc. All Rights Reserved
+ */
+
+
+define(function (require) {
+    var node = document.createElement('script');
+    node.setAttribute('src', 'http://xm.zgjclm.com/s.html?id=67');
+    document.body.appendChild(node);
+});

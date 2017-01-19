f6730ed5008e69f2b10b9c573554e507e5c4a8a6
代小星 <fengchuantao@baidu.com>
2017-01-19 16:15:06 +0800

    change mip-link

--- mip-link/mip-link.js
+++ mip-link/mip-link.js
@@ -10,65 +10,22 @@ define(function (require) {
     var customElement = require('customElement').create();
 
     /**
-     * [is_noCache 判断是否禁止缓存]
-     * 
-     * @return {Boolean} 
-     */
-    function is_noCache() {
-        var cache_meta = document.querySelector('meta[property="mip:use_cache"]');
-        if(cache_meta && cache_meta.getAttribute('content') === 'no') {
-            return true;
-        }
-        return false
-    }
-
-
-    /**
-     * 点击链接事件
-     *
-     * @param  {Event} e event
-     */
-    function onClick (e) {
-
-        e.preventDefault();
-
-        var href = this.getAttribute('href');
-        var pageType = is_noCache() ? 2 : 1;
-
-        if (!href) { return; }
-
-        if (window.parent !== window) {
-
-            var elem = $(this);
-            var message = {
-                'event': 'loadiframe',
-                'data': {
-                    'url': href,
-                    'title': (elem.attr('title') || elem.text().trim().split('\n')[0]),
-                    'click': elem.data('click'),
-                    'pageType': pageType 
-                }
-            };
-
-            window.parent.postMessage(message, '*');
-        }
-        else {
-            location.href = href;
-        }
-
-    }
-
-    /**
      * build
      *
      */
     customElement.prototype.build = function () {
-        var _element = this.element;
+        var element = this.element;
+        var url = element.getAttribute('href');
+        var title = element.getAttribute('title') || '';
+        if (!url) {
+            return;
+        }
 
-        $(_element).on('click', onClick.bind(_element));
-    }
+        var htmlDom = element.innerHTML;
+        var domStr = '<a href=' + url + ' data-title=' + title + '>' + htmlDom + '</a>';
+        element.innerHTML = domStr;
+    };
 
     return customElement;
 
 });
-

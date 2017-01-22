beb44e50d0a692dc7d7b61fddf15d374d595d9f3
wangpei07 <wangpei07@baidu.com>
2017-01-22 17:31:23 +0800

    update: type 名修改

--- mip-ad/mip-ad.js
+++ mip-ad/mip-ad.js
@@ -16,7 +16,7 @@ define(function(require) {
     require('./mip-baidu-wm-ext');
     require('./mip-ad-imageplus');
     require('./mip-ad-ssp');
-    require('./mip-ad-ssprelease');
+    require('./mip-ad-baidussp');
 
     /**
      * render

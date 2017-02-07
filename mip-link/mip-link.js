08e7b16c3696170adebbac5fec29e7c87ecfb675
wangpei07 <wangpei07@baidu.com>
2017-02-07 10:56:01 +0800

    update: mip-link 功能完善

--- mip-link/mip-link.js
+++ mip-link/mip-link.js
@@ -9,63 +9,20 @@ define(function (require) {
 
     var customElement = require('customElement').create();
 
-    /**
-     * [is_noCache 判断是否禁止缓存]
-     * 
-     * @return {Boolean} 
-     */
-    function is_noCache() {
-        var cache_meta = document.querySelector('meta[property="mip:use_cache"]');
-        if(cache_meta && cache_meta.getAttribute('content') === 'no') {
-            return true;
-        }
-        return false
-    }
-
-
-    /**
-     * 点击链接事件
-     *
-     * @param  {Event} e event
-     */
-    function onClick (e) {
-
-        e.preventDefault();
-
-        var href = this.getAttribute('href');
-        var pageType = is_noCache() ? 2 : 1;
-
-        if (!href) { return; }
-
-        if (window.parent !== window) {
-
-            var elem = $(this);
-            var message = {
-                'event': 'loadiframe',
-                'data': {
-                    'url': href,
-                    'title': (elem.attr('title') || elem.text().trim().split('\n')[0]),
-                    'click': elem.data('click'),
-                    'pageType': pageType 
-                }
-            };
-
-            window.parent.postMessage(message, '*');
-        }
-        else {
-            location.href = href;
-        }
-
-    }
 
     /**
      * build
      *
      */
-    customElement.prototype.build = function () {
-        var _element = this.element;
+    customElement.prototype.firstInviewCallback = function () {
+        var element = this.element;
+        var node = element.parentNode;
 
-        $(_element).on('click', onClick.bind(_element));
+        var parent = document.createElement('a');
+        parent.href = element.getAttribute('href');
+        node.replaceChild(parent, element);
+        parent.appendChild(this.element);
+
     }
 
     return customElement;

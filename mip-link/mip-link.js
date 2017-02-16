bdf013fa4a5f70e35a50b12528addfcdb1895bda
wangpei07 <wangpei07@baidu.com>
2017-02-16 18:00:13 +0800

    update: fecs 校验

--- mip-link/mip-link.js
+++ mip-link/mip-link.js
@@ -9,19 +9,19 @@ define(function (require) {
     var customElement = require('customElement').create();
     var util = require('util');
 
-    /**
-     * firstInviewCallback
-     *
-     */
-    
-    function getPaddingOrMargin (elem, style) {
+
+    function getPaddingOrMargin(elem, style) {
         var res = document && document.defaultView
          && document.defaultView.getComputedStyle(elem, null)
          && document.defaultView.getComputedStyle(elem, null)[style];
 
-         return res ? res : '0px';
+        return res ? res : '0px';
     }
 
+    /**
+     * firstInviewCallback
+     *
+     */
     customElement.prototype.firstInviewCallback = function () {
         var element = this.element;
         var node = element.parentNode;
@@ -33,7 +33,7 @@ define(function (require) {
         var parent = document.createElement('a');
         parent.href = element.getAttribute('href');
         parent.classList += classVal.replace('mip-element', '');
-        
+
         element.setAttribute('class', 'mip-element');
         node.replaceChild(parent, element);
         parent.appendChild(element);

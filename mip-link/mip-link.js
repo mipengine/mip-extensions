a5062f18cf9e0a13953693403096effed90506ed
wangpei07 <wangpei07@baidu.com>
2017-02-16 16:25:42 +0800

    update: 修复样式bug

--- mip-link/mip-link.js
+++ mip-link/mip-link.js
@@ -16,16 +16,30 @@ define(function (require) {
     customElement.prototype.firstInviewCallback = function () {
         var element = this.element;
         var node = element.parentNode;
+        var classVal = element.getAttribute('class');
 
         var parent = document.createElement('a');
         parent.href = element.getAttribute('href');
-        util.css(parent, {
-            margin: 0,
-            padding: 0
-        });
+        parent.classList += classVal;
+        
+        element.removeAttribute('class');
         node.replaceChild(parent, element);
         parent.appendChild(element);
 
+        // padding 和 margin 设置，优先考虑 mip-link 的样式设置
+
+        var elementPadding = document.defaultView.getComputedStyle(element, null)['padding'];
+        var elementMargin = document.defaultView.getComputedStyle(element, null)['margin'];
+        var parentPadding = document.defaultView.getComputedStyle(parent, null)['padding'] || 0;
+        var parengMargin = document.defaultView.getComputedStyle(parent, null)['margin'] || 0;
+
+        var paddingValue = elementPadding !== '0px' ? 0 : parentPadding;
+        var marginValue = elementMargin !== '0px' ? 0 : parengMargin;
+        
+        util.css(parent, {
+            margin: marginValue,
+            padding: paddingValue
+        });
     };
 
     return customElement;

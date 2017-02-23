d748a675e8e6544325d24d9d2006819a0484173d
VinLexn <temp@cnode.cn>
2017-02-23 14:16:58 +0800

    站点快商通组件
    定义页面加载快商通组件，实现页面咨询的邀请框等功能。

---
+++ mip-huayi-kst/README.md
@@ -0,0 +1,25 @@
+# mip-huayi-kst
+
+点击加载更多组件
+
+|标题|内容|
+|----|----|
+|类型|事件|
+|支持布局|N/S|
+|所需脚本|https://mipcache.bdstatic.com/static/v1/mip-huayi-kst/mip-huayi-kst.js|
+
+## 示例
+
+
+```
+<mip-huayi-kst data-src="//192.168.0.56:81/onKSTpc.js"></mip-huayi-kst>
+```
+## 属性
+
+### data-src
+说明：指定引用快商通连接地址脚本
+必选项：是
+类型：数值
+取值范围：无
+单位：无
+默认值：无
---
+++ mip-huayi-kst/mip-huayi-kst.js
@@ -0,0 +1,33 @@
+define(function(require) {
+    var GP_ISDEBUG = true;
+    var scriptId = "HY_RES";
+    var customElem = require('customElement').create();
+    customElem.prototype.build = function() {
+        var element = this.element;
+        var jsSrc = element.getAttribute('data-src');
+        initJs(jsSrc);
+        if (GP_ISDEBUG) { console.log('build'); }
+    };
+    /**
+     * [initJs JS初始化函数]
+     * 
+     * @return Append Script
+     */
+    function initJs(_src, _func) {
+        var script = document.createElement('script');
+        script.src = _src;
+        script.charset = 'utf-8';
+        script.onreadystatechange = function() {
+            if (this.readyState == 'complete') {
+                if (typeof _func == "function") { _func(); }
+            }
+
+        }
+        script.onload = function() {
+            if (typeof _func == "function") { _func(); }
+        }
+        document.body.appendChild(script);
+        return script;
+    }
+    return customElem;
+});

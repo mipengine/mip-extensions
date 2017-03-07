0a19713c1ce3c73120f9b799efec4a1fc6e641d6
wangpei07 <wangpei07@baidu.com>
2017-03-07 16:24:05 +0800

    update: 编码规范化

--- mip-infinitescroll/infinitescroll.js
+++ mip-infinitescroll/infinitescroll.js
@@ -4,12 +4,12 @@
  * @date 2017-02-15
  */
 
-define(function () {
+define(function (require) {
     var $ = require('jquery');
-    var InfiniteScroll = function(opt) {
+    var InfiniteScroll = function (opt) {
         if (!opt.$result || !opt.$loading || !opt.pushResult) {
             return;
-        };
+        }
         var me = this;
 
         opt.$result = $(opt.$result);
@@ -60,11 +60,11 @@ define(function () {
                 me.scrollPageCache.content = me.scrollPageCache.content.concat(me._separatePage(me.options.firstResult));
                 me.options.$result.html(me._wrapPageParentDom(me.scrollPageCache.content[0], 0));
                 me.scrollPageCache.topPosition.push(me.options.$result.position().top);
-            };
+            }
 
             // 初始化全局环境变量 && resize时重新获取环境变量
             me.refresh();
-            $(window).on('resize' + me.eventSpace, function() {
+            $(window).on('resize' + me.eventSpace, function () {
                 me.refresh();
             });
 
@@ -74,29 +74,29 @@ define(function () {
             // 如果firstResult不存在,调用_scrollBottomFn方法异步加载首屏数据
             if (!me.options.firstResult.length) {
                 me._scrollBottomFn();
-            };
+            }
         },
 
-        /*
+        /**
          * 重新获取环境变量
          */
-        refresh: function() {
+        refresh: function () {
             var me = this;
 
             // 若为暂停状态,什么也不做
             if (me.state === 'pause') {
                 return;
-            };
+            }
 
             me.wrapperHeight = me.options.$wrapper.height();        // 可视区高度
             me.scrollerHeight = me.options.$scroller.height();      // 滚动容器高度
             me.currentScrollTop = me.options.$wrapper.scrollTop();  // 当前滚动条位置
         },
 
-        /*
+        /**
          * destroy方法
          */
-        destroy: function() {
+        destroy: function () {
             var me = this;
 
             $(window).off('resize' + me.eventSpace);            // 注销resize事件
@@ -105,76 +105,78 @@ define(function () {
             me.scrollPageCache = null;                          // 删除cache数据
         },
 
-        /*
+        /**
          * pause方法,外部接口,用于暂停infiniteScroll
          */
-        pause: function() {
+        pause: function () {
             var me = this;
 
             // 若已经为暂停状态,什么也不做
-            if (me.state == 'pause') {
+            if (me.state === 'pause') {
                 return;
-            };
+            }
 
             // 记录当前滚动位置
             me.pauseScrollTop = me.currentScrollTop;
             me.state = 'pause';
         },
 
-        /*
+        /**
          * start方法,外部接口,用于恢复infiniteScroll
          */
-        start: function() {
+        start: function () {
             var me = this;
 
             // 若已经为执行状态,什么也不做
-            if (me.state == 'start') {
+            if (me.state === 'start') {
                 return;
-            };
-            
+            }
+
             // 恢复滚动位置
             me.options.$wrapper.scrollTop(me.pauseScrollTop);
             me.refresh();
             me.state = 'start';
         },
 
-        /*
+        /**
          * 横屏hack
          * 为保证横屏下滚动容器定位,横竖屏结果高度必须相同
          * 目前百度栅格系统采用流式布局,因此强制横屏下父容器宽度与竖屏相同
          */
-        _horizontalHack: function() {
+        _horizontalHack: function () {
             var me = this;
 
             var verticalScreenWidth;
             if (window.orientation !== undefined) {
                 // 安卓某些系统下screen返回的是高分屏尺寸...
-                if (window.orientation == 0 || window.orientation == 180) {
+                if (window.orientation === 0 || window.orientation === 180) {
                     // 竖屏
                     verticalScreenWidth = Math.min(window.screen.width, $(window).width());
-                } else if (window.orientation == 90 || window.orientation == -90) {
+                }
+                else if (window.orientation === 90 || window.orientation === -90) {
                     // 横屏
                     verticalScreenWidth = Math.min(window.screen.width, window.screen.height);
                     verticalScreenWidth = verticalScreenWidth * $(window).width() / Math.max(window.screen.width, window.screen.height);
                 }
-            } else {
+            }
+            else {
                 // 为防止极个别情况不支持orientation属性(目前未发现)
                 // 当不支持orientation且返回高分屏尺寸时,这里会出bug
                 verticalScreenWidth = Math.min(window.screen.width, window.screen.height);
-            };
+            }
             me.options.$result.css({
                 'max-width': verticalScreenWidth + 'px'
             });
         },
 
-        _bindScroll: function() {
+        _bindScroll: function () {
             var me = this;
 
-            me.options.$wrapper.on('scroll' + me.eventSpace, function() {
+            me.options.$wrapper.on('scroll' + me.eventSpace, function () {
                 // 若为暂停状态,什么也不做
                 if (me.state === 'pause') {
                     return;
-                };
+                }
 
                 // 获取当前滚动条位置
                 me.currentScrollTop = $(this).scrollTop();
@@ -185,14 +187,14 @@ define(function () {
                 if (me.currentScrollTop <= 0) {
                     // 执行回调
                     me.options.onScrollTop && me.options.onScrollTop.call(me);
-                };
+                }
 
                 // 到底了
                 if (me.currentScrollTop >= me.scrollerHeight - me.wrapperHeight - me.options.bufferHeightPx) {
                     me._scrollBottomFn();
                     // 执行回调
                     me.options.onScrollBottom && me.options.onScrollBottom.call(me);
-                };
+                }
 
                 // 获取当前可视区页码
                 var currentShowPage = me.getShowPage();
@@ -204,20 +206,20 @@ define(function () {
                     // 清理or回填dom
                     if (me.options.limitShowPn) {
                         me._cycleScrollElement(currentShowPage);
-                    };
-                };
+                    }
+                }
             });
 
             // 若初始即不满一屏,trigger scroll事件触发加载
             if (me.currentScrollTop >= me.scrollerHeight - me.wrapperHeight - me.options.bufferHeightPx) {
                 me.options.$wrapper.trigger('scroll');
-            };
+            }
         },
 
-        /*
+        /**
          * 当滚动条滚动到页面底部时执行
          */
-        _scrollBottomFn: function() {
+        _scrollBottomFn: function () {
             var me = this;
 
             var pn = me.currentLoadPage + 1;                // 需要加载的页码(从0计)
@@ -229,34 +231,36 @@ define(function () {
                 me._updateScrollElement(pn);
                 // 执行回调
                 me.options.onLoadNewPage && me.options.onLoadNewPage.call(me, pn);
-            };
+            }
             // 数据不够 && 数据状态为默认(!无数据 && !请求中 && !请求失败)
-            if (me.dataStatus == 1 && pn + me.options.preLoadPn >= dn) {
+            if (me.dataStatus === 1 && pn + me.options.preLoadPn >= dn) {
                 // 调用cb:pushResult请求新数据,由于数据请求一般为异步,使用标准Deferred对象处理(同时也兼容同步数据返回)
-                var dataDeferred = me.options.pushResult.call(me, (dn+1)*me.options.pageResultNum, dn-pn);
+                var dataDeferred = me.options.pushResult.call(me, (dn + 1) * me.options.pageResultNum, dn - pn);
                 // 标记数据状态为请求中
                 me.dataStatus = 2;
                 $.when(dataDeferred).then(
                     // 成功
                     function (newResultArr) {
                         // 处理新增数据
-                        if (newResultArr == 'NULL') {
+                        if (newResultArr === 'NULL') {
                             // 标记数据状态为无数据
                             me.dataStatus = 0;
                             me.options.$loading.html(me.options.loadOverHtml);
-                        } else if (newResultArr.length) {
+                        }
+                        else if (newResultArr.length) {
                             // 标记数据状态为默认
                             me.dataStatus = 1;
                             // 将新数据合并入数据缓存中
-                            me.scrollPageCache.content = me.scrollPageCache.content.concat(me._separatePage(newResultArr));
+                            me.scrollPageCache.content
+                                = me.scrollPageCache.content.concat(me._separatePage(newResultArr));
                             // trigger scroll事件,确保继续触发数据加载
                             me.options.$wrapper.trigger('scroll');
-                        };
+                        }
                     // 失败
                     }, function () {
                         // 标记数据状态为请求失败
                         me.dataStatus = 3;
-                        me.options.$loading.html(me.options.loadFailHtml).one('click' + me.eventSpace, function() {
+                        me.options.$loading.html(me.options.loadFailHtml).one('click' + me.eventSpace, function () {
                             // 标记数据状态为默认
                             me.dataStatus = 1;
                             me.options.$loading.html(me.options.loadingHtml);
@@ -265,13 +269,15 @@ define(function () {
                         });
                     }
                 );
-            };
+            }
         },
 
-        /*
+        /**
          * 按页更新滚动元素内容
+         *
+         * @param  {integer} pn 页码
          */
-        _updateScrollElement: function(pn) {
+        _updateScrollElement: function (pn) {
             var me = this;
 
             var $dom_newPage = $(me._wrapPageParentDom(me.scrollPageCache.content[pn], pn));
@@ -283,16 +289,18 @@ define(function () {
             me.scrollPageCache.topPosition.push($dom_newPage.position().top);
         },
 
-        /*
+        /**
          * 清理&恢复dom方法
          * IP:[number]当前可视区页码
          * 由于wise性能较差，需要清理掉滚动到可视区外的元素
+         *
+         * @param  {integer} pn 页码
          */
-        _cycleScrollElement: function(pn) {
+        _cycleScrollElement: function (pn) {
             var me = this;
 
             var recycleClass = 'infinite-recycle';
-            var startPage = Math.max(pn - Math.floor((me.options.limitShowPn-1)/2), 0);
+            var startPage = Math.max(pn - Math.floor((me.options.limitShowPn - 1) / 2), 0);
             // 获取所有结果列表dom
             var $dom_resultElement = me.options.$result.find('.' + me.options.scrollPageClass);
             // 选出当前需要被显示的dom页
@@ -301,48 +309,55 @@ define(function () {
             // todo:这里应该还有优化空间
             if ($dom_shouldShowElement.length) {
                 // 恢复:在应该被显示的dom中选出所有带回收标记标签的元素执行恢复操作
-                $dom_shouldShowElement.each(function(){
+                $dom_shouldShowElement.each(function () {
                     if ($(this).hasClass(recycleClass)) {
                         $(this).html(me.scrollPageCache.content[$(this).attr('data-page')]);
                         $(this).removeClass(recycleClass);
-                    };
+                    }
                 });
                 // 清理:选出所有不应该被显示的dom,并排除已有回收标记标签的元素,执行清理操作
-                $dom_resultElement.not($dom_shouldShowElement).not('.' + recycleClass).each(function() {
+                $dom_resultElement.not($dom_shouldShowElement).not('.' + recycleClass).each(function () {
                     $(this).height($(this).height()).empty();
                     $(this).addClass(recycleClass);
                 });
                 // 这里有可能导致整体高度变化,需要重新更新高度
                 me.scrollerHeight = me.options.$scroller.height();
-            };
+            }
         },
 
-        /*
+        /**
          * 将结果处理成分页的数组结构返回
          * IP:[arr]结果列表html代码片段
          * OP:[arr]按页分割的html代码片段
+         *
+         * @param  {Array} listArr 结果
+         * @return {Array}   html array
          */
-        _separatePage: function(listArr) {
+        _separatePage: function (listArr) {
             var me = this;
 
             if (!listArr.length) {
                 return;
-            };
+            }
             var pageResultNum = me.options.pageResultNum;               // 每页结果数
             var pageNum = Math.ceil(listArr.length / pageResultNum);    // 分成x页
             var pageHtmlArr = [];
             for (var i = 0; i < pageNum; i++) {
-                pageHtmlArr.push(listArr.slice(i*pageResultNum, i*pageResultNum+pageResultNum).join(''));
-            };
+                pageHtmlArr.push(listArr.slice(i * pageResultNum, i * pageResultNum + pageResultNum).join(''));
+            }
             return pageHtmlArr;
         },
 
-        /*
+        /**
          * 为每页内容包裹父容器
          * IP:html-[string]一页的html代码片段;pn-[number]当前页码
          * OP:[string]按页包裹完每页父容器的html代码
+         *
+         * @param  {string} html html
+         * @param  {integer} pn 页码
+         * @return {string}   拼接好的html
          */
-        _wrapPageParentDom: function(html, pn) {
+        _wrapPageParentDom: function (html, pn) {
             var me = this;
 
             return (
@@ -354,18 +369,20 @@ define(function () {
             );
         },
 
-        /*
+        /**
          * 获取当前可视区页码的方法(从0计)
+         *
+         * @return {integer} 0 或 1
          */
-        getShowPage: function() {
+        getShowPage: function () {
             var me = this;
 
             var scrollPageCacheTopPosition = me.scrollPageCache.topPosition.concat();
             for (var i = scrollPageCacheTopPosition.length - 1; i >= 0; i--) {
                 if (me.currentScrollTop >= scrollPageCacheTopPosition[i]) {
                     return i;
-                };
-            };
+                }
+            }
             return 0;
         },
 

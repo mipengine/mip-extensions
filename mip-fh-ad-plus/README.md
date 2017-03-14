5d3e550f68ce337773226518f2ac05ed9a4a8f9d
laoono <laoono@163.com>
2017-03-14 13:46:43 +0800

    mip-fh-ad-plus 增加支持投放医生广告、增加直销广告请求合并

--- mip-fh-ad-plus/README.md
+++ mip-fh-ad-plus/README.md
@@ -13,10 +13,35 @@ mip-fh-ad-plus 用来支持整站页面的直投广告显示
 在MIP HTML中,直接使用标签, 用于正常显示直投的广告。示例如下:
 
 ```html
+    <style mip-custom>
+    .dn {display: none;}
+    </style>
+    
+    <meta name="fh-ad-keywords" content="射精,阳痿,早泄">
+    
+    <div class="dn" fh-ad-empty-show>没有直销广告，才会显示</div>
+    
     <div>
-        <mip-fh-ad-plus fh-ad-pid="19"></mip-fh-ad-plus>
+        <mip-fh-ad-plus fh-ad-pid="48" combo></mip-fh-ad-plus>
+    </div>
+    
+    <div>
+        <mip-fh-ad-plus fh-ad-pid="55"></mip-fh-ad-plus>
+    </div>
+    
+    <div>
+        <mip-fh-ad-plus fh-ad-pid="1"></mip-fh-ad-plus>
+    </div>
+    
+    <div>
+        <mip-fh-ad-plus fh-ad-pid="9" fh-ad-uid="1119181"></mip-fh-ad-plus>
+    </div>
+    
+    <div>
+        <mip-fh-ad-plus fh-ad-pid="9" fh-ad-uid="111"></mip-fh-ad-plus>
     </div>
 ```
+
 
 ## 属性
 
@@ -32,9 +57,24 @@ mip-fh-ad-plus 用来支持整站页面的直投广告显示
 说明：飞华直投广告关键词组,多词用,分隔例:`fh-ad-keywords="瘦身,性病,减肥"`, 如果没有指定则直接获取`meta[name="fh-ad-keywords"]`的`content`属性值
 必填：否
 
+### fh-ad-uid
+
+说明：飞华广告位医生uid
+必填：否
+格式：数字
+单位：无
+
 ### lazy
 
 说明：懒加载(lazy) (true | false)
+必填：否
+格式：boolean
+取值：true/false
+默认值：false
+
+### combo
+
+说明：一次性绑定所有直销广告请求 (true | false)
 必填：否
 格式：boolean
 取值：true/false
--- mip-fh-ad-plus/mip-fh-ad-plus.js
+++ mip-fh-ad-plus/mip-fh-ad-plus.js
@@ -10,6 +10,7 @@
 define(function (require) {
 
     var $ = require('zepto');
+    var util = require('util');
 
     var customElem = require('customElement').create();
     var $body = $('body');
@@ -20,7 +21,7 @@ define(function (require) {
     var paramObj = $adKeywords.attr('content');
 
     // 直投广告请求url
-    var adUrl = 'https://partners.fh21.com.cn/partners/showcodejsonp?callback=?';
+    var adUrl = 'http://partners.sandbox.fh21.com.cn/partners/showcodejsonp?callback=?';
 
     // 初始化直 投广告
     var init = function (opt) {
@@ -29,6 +30,9 @@ define(function (require) {
         var posId = [opt.posId] || [1];
         var kw = opt.kw || '';
         var element = opt.element;
+        var uid = opt.uid;
+        var combo = Boolean(opt.combo);
+        var $plus = $('mip-fh-ad-plus');
 
         // 接口参数值
         var query = {
@@ -36,63 +40,133 @@ define(function (require) {
             pid: posId.join(',')
         };
 
-        var FhAdPutNum = $('mip-fh-ad-plus').length;
+        uid && (query.uid = uid);
 
-        // 判断直投广告参数,是否加载直投广告请求
-        if (kw && kw.length) {
-            $.getJSON(adUrl, query, function (res) {
-                var fhAdNum = $body.attr('fh-ad-num') || 0;
+        var FhAdPutNum = $plus.length;
 
-                var data = $.parseJSON(res.result);
-
-                // 遍历直投广告ID
-                $.each(data, function (k, v) {
-                    // 有特定广告位id的直投广告 先隐藏网盟 再显示直投
-                    if ($.trim(v)) {
-                        element.children(':first-child').remove();
-                        element.html(v);
-
-                        $body.addClass('view-fh-ad-' + (+k));
-                        $body.attr('fh-ad-num', ++fhAdNum);
-                    }
-                    // 无特定广告位id直投广告显示网盟
-                    else {
-                        element.children(':first-child').show();
-
-                        $body.addClass('view-fh-ad-' + (+k) + '-union');
-                        $body.attr('fh-ad-num', --fhAdNum);
-                    }
-                    // 所有的直投广告位均无直投广告
-                    if (fhAdNum === -FhAdPutNum) {
-                        $emptyShowEle.show();
-                    }
-                });
-            });
+        if (combo) {
+            query.pid = getPids($plus).join(',');
+            query.uid = getUids($plus).join(',');
         }
-        // 无任何直投广告位
         else {
-            $body.addClass('view-fh-ad-union');
-            $emptyShowEle.show();
+            if (getCombo($plus).length !== 0) {
+                return;
+            }
         }
+
+        $.getJSON(adUrl, query, function (res) {
+            var isHasFhAd;
+            var fhAdNum = $body.attr('fh-ad-num') || 0;
+            var data = $.parseJSON(res.result);
+
+            // 遍历直投广告ID
+            $.each(data, function (k, v) {
+
+                // 判断是否触发一次性所有广告位请求
+                if (combo) {
+                    element = $('mip-fh-ad-plus[fh-ad-pid="' + k + '"]');
+                    uid = element.attr('fh-ad-uid');
+                }
+
+                // 根据医生id判断物料类型
+                v = (query.uid && util.fn.isPlainObject(v)) ? v[uid] : v;
+
+                // 有特定广告位id的直投广告 先隐藏网盟 再显示直投
+                if ($.trim(v)) {
+                    element.children(':first-child').remove();
+                    element.html(v);
+
+                    $body.addClass('view-fh-ad-' + (+k));
+                    $body.attr('fh-ad-num', ++fhAdNum);
+
+                    combo && (isHasFhAd = true);
+                }
+                // 无特定广告位id直投广告显示网盟
+                else {
+                    element.children(':first-child').show();
+
+                    $body.addClass('view-fh-ad-' + (+k) + '-union');
+                    $body.attr('fh-ad-num', --fhAdNum);
+                }
+
+                // 所有的直投广告位均无直投广告
+                if (fhAdNum === -FhAdPutNum || (combo && !isHasFhAd)) {
+                    $body.addClass('view-fh-ad-union');
+                    $emptyShowEle.show();
+                }
+            });
+        });
     };
 
-    var getOpt = function (element) {
+    function getOpt(element) {
         var $element = $(element);
-        // 获取元素绑定的广告位id和关键词
+        // 获取元素绑定的广告位id、uid(医生id)和关键词
         var posId = $element.attr('fh-ad-pid');
         var keywords = $element.attr('fh-ad-keywords') || paramObj;
         var lazy = $element.attr('lazy') || 'false';
+        var uid = $element.attr('fh-ad-uid');
 
         // 广告初始化参数
-        // 广告位id数组 [1, 11, 14, 47, 48, 49];
-
         var opt = {
-            posId: posId,
+            posId: $.trim(posId),
             kw: $.trim(keywords),
             lazy: lazy,
+            uid: $.trim(uid),
+            combo: (isCombo($element)) || false,
             element: $element
         };
+
         return opt;
+    }
+
+    function getPids(ele) {
+        var res = [];
+
+        $.each(ele, function (k, v) {
+            res.push($(v).attr('fh-ad-pid'));
+        });
+
+        return res;
+    }
+
+    function getCombo(ele) {
+        var res = [];
+
+        $.each(ele, function (k, v) {
+            var combo = $(v).attr('combo');
+            if (isCombo(v)) {
+                res.push(combo);
+            }
+        });
+
+        return res;
+    }
+
+    function isCombo(ele) {
+        var flag;
+        var combo = $(ele).attr('combo');
+
+        if (typeof combo === 'string' && combo !== 'false') {
+            flag = true;
+        }
+        else {
+            flag = false;
+        }
+
+        return flag;
+    }
+
+    var getUids = function (ele) {
+        var res = [];
+
+        $.each(ele, function (k, v) {
+            var combo = $(v).attr('fh-ad-uid');
+            if (combo) {
+                res.push(combo);
+            }
+        });
+
+        return res;
     };
 
     // build 方法，元素插入到文档时执行，仅会执行一次

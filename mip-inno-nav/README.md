b22542c4dbefb072aeb4a8c23d5bbd3a7d59f3ed
James <zhurizhe71@gmail.com>
2017-03-16 11:26:23 +0800

    更新界面和事件绑定

--- mip-inno-nav/README.md
+++ mip-inno-nav/README.md
@@ -1,73 +1,31 @@
 # mip-inno-nav
 
-根据mip-nav-slidedown修改，增加两个属性， 实现innotopia的个性化响应式的菜单
+ 实现innotopia的个性化响应式的菜单
 
 标题|内容
 ----|----
 类型|通用
 支持布局|responsive,fixed-height,fill,container,fixed
-所需脚本|https://mipcache.bdstatic.com/static/v1/mip-inno-nav/mip-inno-nav.js
+
 
 ## 示例
 ```html
-<div class="mip-nav-wrapper">
-<mip-inno-nav data-id="bs-navbar" class="mip-element-sidebar container" data-showbrand="1" data-btn-pos="left" data-bg-color="#f69927" data-fg-color="#fff">
-    <nav id="bs-navbar" class="navbar-collapse collapse navbar navbar-static-top">
-        <ul class="nav navbar-nav navbar-right">
-            <li class="index-body">
-                <a target="_blank" href="//www.mipengine.org/">首页</a>
-            </li>
-            <li class="doc-body">
-                <a target="_blank" href="//www.mipengine.org/doc/00-mip-101.html">产品</a>
-            </li>
-            <li class="timeline-body">
-                <a target="_blank" href="//www.mipengine.org/timeline.html">案例</a>
-            </li>
-            <li class="">
-                <a target="_blank" href="http://www.cnblogs.com/mipengine/" target="_blank">博客</a>
-            </li>
-            <li class="navbar-wise-close">
-                <span id="navbar-wise-close-btn"></span>
-            </li>
-        </ul>
-    </nav>
+
+<mip-inno-nav layout="responsive" width="640" height="800">
+        <span class="menu"></span>
+
+      <menu>
+                  <span class="close"></span>
+                  <div class="logo"></div>
+                  <mip-link href="http://m.innotopia.cn/index.html" target="_blank">首页</mip-link>
+                  <hr/>
+                  <mip-link href="http://m.innotopia.cn/product.html">产品</mip-link>
+                  <hr/>
+                  <mip-link href="http://m.innotopia.cn/cases.html">案例</mip-link>
+                  <hr/>
+                  <mip-link href="tel:4001338787">联系我们</mip-link>
+              </menu>
 </mip-inno-nav>
-</div>
+
 ```
 
-## 属性
-
-### data-id  
-说明：内部菜单id  
-必选项：是  
-类型：字符串  
-
-### data-showbrand  
-说明：是否需要左上角显示可点击区域  
-必选项：否  
-类型：数字  
-取值：0（不显示），1（显示）  
-默认值：1
-
-### data-brandname  
-说明：左上角显示可点击区域文字，仅在data-showbrand=1时显示  
-必选项：否  
-类型：字符串，如"MIP官网"  
-
-### data-brandhref
-说明：左上角图标跳转链接，在data-showbrand为1时有效
-必选项：否
-类型：url
-默认：'/'
-
-### data-bg-color
-说明：菜单的背景颜色
-必选项：否
-类型：字符串
-默认：'white'
-
-### data-fg-color
-说明：菜单项文字颜色
-必选项：否
-类型：字符串
-默认：'black'
--- mip-inno-nav/mip-inno-nav.js
+++ mip-inno-nav/mip-inno-nav.js
@@ -11,7 +11,6 @@ define(function (require) {
         var element = this.element;
         render(element);
         bindEvents(element);
-        showNavBar(element);
     }
 
     /**
@@ -20,30 +19,7 @@ define(function (require) {
      * @param  {obj} me this
      */
     function render(me) {
-        var $this = $(me);
-        var id = $this.data('id');
-        var showBrand = !($this.data('showbrand') === 0);
-        var brandName = $this.data('brandname') || '';
-        var brandHref = $this.data('brandhref') || '#';
-        var btnPos = $this.data('btn-pos') || 'left';
-        var bgColor = $this.data('bg-color') || 'white';
-        var fgColor = $this.data('fg-color') || 'black';
-        var $ulNav = $this.find('#' + id);
-        var $container = $('<div></div>');
-        var $btnWrap = '<div class="navbar-header">'
-            + '<button class="navbar-toggle collapsed" type="button" data-target="#'
-            + id + '" aria-controls="' + id + '" aria-expanded="false" style="float:'+btnPos+'">'
-            + '<span class="sr-only">导航</span>'
-            + '<span class="icon-bar"></span>'
-            + '<span class="icon-bar"></span>'
-            + '<span class="icon-bar"></span>'
-            + '</button>'
-            + (showBrand ? '<a href=' + brandHref + ' class="navbar-brand">' + brandName + '</a>' : '')
-            + '</div>';
-        $container.append($btnWrap).append($ulNav).appendTo($this);
-        $('.mip-nav-wrapper nav').css('background-color',bgColor);
-        $('.mip-nav-wrapper nav a').css('color',fgColor);
-        $('.mip-nav-wrapper').addClass('show');
+       // todo render
     }
 
     /**
@@ -51,114 +27,19 @@ define(function (require) {
      *
      * @param  {obj} me this
      */
-    function bindEvents(me) {
-        var bodyClass = $('body').attr('class');
-        $('#bs-navbar').find('.' + bodyClass).addClass('active');
+    function bindEvents() {
 
-        $(document).on('click', '.navbar-header .navbar-toggle', navClickHandler);
-
-        $('#bs-navbar .navbar-nav li').on('click', function () {
-            $('.navbar-header .navbar-toggle').trigger('click');
+        $('span.menu').on('click', function () {
+            $('menu').show();
         });
-        // 主菜单关闭按钮 touchstart touchend mousedown mouseup变色
-        addHoverClass($('#navbar-wise-close-btn'));
-        $('#navbar-wise-close-btn').on('touchend', function (e) {
-            $('.navbar-header .navbar-toggle').trigger('click');
-            e.preventDefault();
-            e.stopPropagation();
-        }).on('click', function () {
-            $('.navbar-header .navbar-toggle').trigger('click');
+        $('span.close').on('click', function () {
+            $('menu').hide();
         });
+
     }
 
 
-    /**
-     * 展开关闭菜单效果
-     *
-     * @param  {event} e click event
-     */
-    function navClickHandler(e) {
-        if (window.innerWidth > 767) {
-            return;
-        }
 
-        var $wiseNav = $('#bs-navbar');
-
-        if ($wiseNav.hasClass('in')) {
-            // 关闭菜单
-            $wiseNav.height('0px');
-            $('body').css('overflow', 'scroll');
-            $('.navbar-wise-close').css('margin-top', '20px');
-            $(window).off('orientationchange');
-            $('html').add($('body')).removeClass('noscroll');
-            setTimeout(function () {
-                $wiseNav.removeClass('in');
-            }, 500);
-        }
-        else {
-            var closeBtnTop = 20;
-            $wiseNav = $('#bs-navbar');
-
-            $('html').add($('body')).addClass('noscroll');
-            setNavHeight('open');
-
-            $(window).on('orientationchange', function () {
-                window.setTimeout(function () { // hack: orientationchange 取window高度不及时
-                    setNavHeight('resize');
-                }, 100);
-            }).on('resize', function () {
-                setNavHeight('resize');
-            });
-        }
-
-        /**
-         * 计算窗口高度
-         *
-         * @param {string} mode 用于区分模式
-         */
-        function setNavHeight(mode) {
-            if (mode === 'open') {
-                $wiseNav.addClass('in');
-            }
-
-            if (mode === 'resize' && $wiseNav.hasClass('in') || mode === 'open') {
-                $wiseNav.height(window.innerHeight - $('.navbar-header').height());
-                closeBtnTop = window.innerHeight - 50 - ($('.navbar-right .index-body').height() + 20) * 4 - 90;
-                if (closeBtnTop > 20) {
-                    $('.navbar-wise-close').css('margin-top', closeBtnTop + 'px');
-                }
-                else {
-                    $('.navbar-wise-close').css('margin-top', '20px');
-                }
-            }
-        }
-    }
-
-    /**
-     * 增加按钮按下class对应颜色
-     *
-     * @param {obj} $dom dom object
-     */
-    function addHoverClass($dom) {
-        $dom.on('touchstart', function () {
-            // 按钮按下时，改变颜色
-            $(this).addClass('down');
-        }).on('mousedown', function () {
-            // 鼠标按下时，改变颜色
-            $(this).addClass('down');
-        }).on('touchend', function () {
-            // 按钮抬起时，改变颜色*2，收起菜单
-            $(this).removeClass('down');
-        }).on('mouseup', function () {
-            // 鼠标抬起时，改变颜色*2，收起菜单
-            $(this).removeClass('down');
-        });
-    }
-
-    function showNavBar(me) {
-        var $this = $(me);
-        $this.removeAttr('style');
-    }
 
     // build 方法，元素插入到文档时执行，仅会执行一次
     customElement.prototype.build = build;

8ec8598e075f9c38138d22cf02b00c45ec612956
Amada <943656412@qq.com>
2017-04-13 09:19:00 +0800

    fecs规范

--- mip-xzw-article/mip-xzw-article.js
+++ mip-xzw-article/mip-xzw-article.js
@@ -8,7 +8,7 @@ define(function (require) {
     var $ = require('zepto');
     var isShowStar = false;
     var customElem = require('customElement').create();
-    var adata = [['aries','白羊','3.21-4.19','热情活力',2],['taurus','金牛','4.20-5.20','稳健固执',3],['gemini','双子','5.21-6.21','花心多变',4],['cancer','巨蟹','6.22-7.22','敏感柔情',5],['leo','狮子','7.23-8.22','骄傲威严',14],['virgo','处女','8.23-9.22','完美理性',15],['libra','天秤','9.23-10.23','公平和谐',16],['scorpio','天蝎','10.24-11.22','神秘敏锐',17],['sagittarius','射手','11.23-12.21','自由乐观',18],['capricorn','摩羯','12.22-1.19','执著现实',19],['aquarius','水瓶','1.20-2.18','自由博爱',20],['pisces','双鱼','2.19-3.20','浪漫梦幻',21]];
+    var adata = ['白羊', '金牛', '双子', '巨蟹', '狮子', '处女', '天秤', '天蝎', '射手', '摩羯', '水瓶', '双鱼'];
     customElem.prototype.build = function () {
         var starBox = $('.xz_select');
         var starbg = $('.bg_black');
@@ -38,7 +38,7 @@ define(function (require) {
             aid = 1;
         }
         aid = aid - 1;
-        $('title').text(adata[aid][1]);
+        $('title').text(adata[aid]);
         $.ajax({
             url: 'http://cache.xzw.com/mip/data.js',
             dataType: 'jsonp',

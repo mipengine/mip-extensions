047b37703c15ac33664851f34b3b93f39cdc4c72
zhanghaitao <zhanghaitao@xywy.com>
2017-05-02 10:49:41 +0800

    增加一个小科室id

--- mip-wkad-config/mip-wkad-config.js
+++ mip-wkad-config/mip-wkad-config.js
@@ -2,8 +2,8 @@
 * 寻医问药mip改造 广告配置组件
 * @file 脚本支持
 * @author jqthink@gmail.com
-* @time 2017.04.27
-* @version 1.0.3
+* @time 2017.05.02
+* @version 1.0.4
 */
 define(function (require) {
     var $ = require('zepto');
@@ -29,8 +29,12 @@ define(function (require) {
         var department = $(elem).attr('department');
         var paramId = $(elem).attr('param');
         var departId = $(elem).attr('depart_id');
+        var departSid = $(elem).attr('depart_sid');
         if(departId){
             window['subject_pid'] = departId;
+        }
+        if(departSid){
+            window['subject'] = departSid;
         }
         switch (attr) {
             case 'take_ip':

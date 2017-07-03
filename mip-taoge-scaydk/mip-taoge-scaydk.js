9388c26a4ae965c8bf73a66c6b1b6e5dcc61751d
liangtao <liangtao.gz@foxmail.com>
2017-07-03 15:05:19 +0800

    inmit

---
+++ mip-taoge-scaydk/mip-taoge-scaydk.js
@@ -0,0 +1,18 @@
+/**
+ * @file mip-taoge-scaydk 组件
+ * @author
+ */
+
+define(function (require) {
+
+    var customElement = require('customElement').create();
+
+    /**
+     * 构造元素，只会运行一次
+     */
+    customElement.prototype.build = function () {
+        // TODO
+    };
+
+    return customElement;
+});

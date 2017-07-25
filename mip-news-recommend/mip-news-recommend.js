601c79e3e34597829e9bb7f93a54dc2872e58229
wupengFEX <smartfutureplayer@gmail.com>
2017-07-25 15:12:11 +0800

    fixed reg

--- mip-news-recommend/mip-news-recommend.js
+++ mip-news-recommend/mip-news-recommend.js
@@ -6,7 +6,6 @@
 
 define(function (require) {
     var $ = require('zepto');
-    var util = require('util');
     var viewer = require('viewer');
     var RecommendElement = require('customElement').create();
     var recommend;
@@ -35,7 +34,15 @@ define(function (require) {
     function getOriginUrl() {
         var url = location.href;
 
-        url = util.parseCacheUrl(url);
+        if (/mipcache\./g.test(url)) {
+            url = url.replace(/^http(s?)\:\/\/mipcache.bdstatic.com\/c\/(s?)/g, function ($0, $1, $2) {
+                return $2 === 's' ? 'https://' : 'http://';
+            });
+        } else if (/mipcdn.com\./g.test(url)) {
+            url = url.replace(/^http(s?)\:\/\/.*.mipcdn.com\/c\/(s?)/g, function ($0, $1, $2) {
+                return $2 === 's' ? 'https://' : 'http://';
+            });
+        }
         url = url.replace(/\#.*$/g, '');
 
         return url;

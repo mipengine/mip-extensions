d50126e5b9b936d31130466d03e750302a3d882a
hejieye <ishejieye@126.com>
2017-08-10 10:07:31 +0800

    Update mip-iask-business.js

--- mip-iask-business/mip-iask-business.js
+++ mip-iask-business/mip-iask-business.js
@@ -653,7 +653,7 @@ define(function (require) {
     };
     // 南方网通底部悬浮广告
     var southnetwork = function (openId, div) {
-        var url = 'http://imgv2.g3user.com/api/b.php?uid=' + openId + '&type=m&callback=?';
+        var url = 'https://imgv2-ssl.g3user.com/api/b.php?uid=' + openId + '&type=m&callback=?';
         try {
             $.getJSON(url,
             function (data) {

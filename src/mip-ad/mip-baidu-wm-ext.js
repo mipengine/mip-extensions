660e054558870c5c558896cc6f3c54999d13c92e
lilangbo <lilangbo@baidu.com>
2017-11-16 15:26:31 +0800

    add sample logic

--- src/mip-ad/mip-baidu-wm-ext.js
+++ src/mip-ad/mip-baidu-wm-ext.js
@@ -1,45 +1,62 @@
 /**
- * 广告扩展组件
- * 
- * @author wangpei07@baidu.com
+ * @file 广告扩展组件
+ * @author lilangbo@baidu.com
  * @version 1.0
  * @copyright 2016 Baidu.com, Inc. All Rights Reserved
  */
 
 define(function (require) {
-    
-    var render = function(_this, me) {
+    var render = function (that, me) {
 
-        var self = _this;
+        var self = that;
         var domain = self.getAttribute('domain');
         var token = self.getAttribute('token');
+        var exps = '113002';
 
-        if(domain && token) {
+        var sample = MIP.hash.get('sample');
+        if (sample === 'wm-ad-sam') {
+            exps = '113004';
+        }
 
-            var script = document.createElement('script');
-            script.src = document.location.protocol + '//' + domain + '/' + token + '.js';
-            document.body.appendChild(script);
-
-            var fixedElement = require('fixed-element');
-            var layer = fixedElement._fixedLayer;
-            var child = document.getElementById(token);
-            
-            child.addEventListener("DOMSubtreeModified", function(e) {
-                var elem = window.getComputedStyle(child, null);
-                var pos = elem && elem.getPropertyValue('position') ? 
-                          elem.getPropertyValue('position') : '';
-                if(layer && layer.querySelector('#'+token)){
-                  return;
+        if (domain && token) {
+            // 判断 preload 逻辑
+            var scripts = document.querySelector('script[mip-preload="mip-script-wm"]');
+            if (scripts) {
+                var apiStr = '__container_api_';
+                (window[apiStr] = window[apiStr] || []).push({
+                    containerId: token,
+                    exps: exps,
+                    token: token
+                });
+            }
+            else {
+                window['_' + token] = {
+                    exps: exps
                 }
-                if(pos == 'fixed' && layer) {
-                    var idx = document.querySelectorAll('mip-fixed').length;
-                    var data = {
-                        element: child.parentElement,
-                        id: 'Fixed'+ idx
-                    };
-                    fixedElement.moveToFixedLayer(data, parseInt(idx));
-                }
-            },false);
+                var script = document.createElement('script');
+                script.src = document.location.protocol + '//' + domain + '/' + token + '.js';
+                document.body.appendChild(script);
+
+                var fixedElement = require('fixed-element');
+                var layer = fixedElement._fixedLayer;
+                var child = document.getElementById(token);
+                child.addEventListener('DOMSubtreeModified', function (e) {
+                    var elem = window.getComputedStyle(child, null);
+                    var pos = elem && elem.getPropertyValue('position')
+                        ? elem.getPropertyValue('position') : '';
+                    if (layer && layer.querySelector('#' + token)) {
+                        return;
+                    }
+                    if (pos === 'fixed' && layer) {
+                        var idx = document.querySelectorAll('mip-fixed').length;
+                        var data = {
+                            element: child.parentElement,
+                            id: 'Fixed' + idx
+                        };
+                        fixedElement.moveToFixedLayer(data, parseInt(idx, 10));
+                    }
+                }, false);
+            }
 
         } else {
             console.error('请输入正确的 domain 或者 token');
@@ -48,5 +65,5 @@ define(function (require) {
 
     return {
         render: render
-    }
+    };
 });

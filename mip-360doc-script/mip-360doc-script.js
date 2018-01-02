861d74471ee389341d6e9f253253cddf779cbd7b
360doc <caizhi@360doc.com>
2018-01-02 16:31:12 +0800

    mip-360doc-script.js 广告组件修改

--- mip-360doc-script/mip-360doc-script.js
+++ mip-360doc-script/mip-360doc-script.js
@@ -16,7 +16,7 @@ define(function (require) {
         tanxh.insertBefore(node, tanxh.firstChild);
     }
     // build 方法，元素插入到文档时执行，仅会执行一次
-    customElem.prototype.build = function () {
+    customElem.prototype.build = function () {       
         if ($('.mip-360doc-script-wxggalink') !== null) {
             $('.mip-360doc-script-wxggalink').html('<span class=\'mip-360doc-script-pic\'><img src=\'https://pubimage.360doc.com/transfer/images/zhaishou2.png\' class=\'pic2\'/></span><span class=\'mip-360doc-script-pic\'><img src=\'https://pubimage.360doc.com/transfer/images/xiazai2.png\'  class=\'pic2\'/></span>');
             var picn = $('.mip-360doc-script-pic').length;

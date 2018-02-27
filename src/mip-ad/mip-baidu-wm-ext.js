a6a5e2be851bdd4fdd6f719ab0556131ce0e09f8
lilangbo <lilangbo@baidu.com>
2018-02-27 14:15:04 +0800

    feat:转全反屏蔽的广告优化，上线普通广告的小流量实验

--- src/mip-ad/mip-baidu-wm-ext.js
+++ src/mip-ad/mip-baidu-wm-ext.js
@@ -13,18 +13,11 @@ define(function (require) {
         var token = self.getAttribute('token');
         var exps = '112002';
         var MIP = window.MIP || {};
-        var sample = MIP.hash.get('sample');
-        if (sample === 'mip-wm-base') {
-            exps = '117001';
-        }
-        else if (sample === 'mip-wm-exp') {
-            exps = '117011';
-        }
 
         if (domain && token) {
             // 判断 preload 逻辑
             var scripts = document.querySelector('script[mip-preload="mip-script-wm"]');
-            if (scripts && sample === 'mip-wm-exp') {
+            if (scripts) {
                 var apiStr = '__container_api_';
                 (window[apiStr] = window[apiStr] || []).push({
                     containerId: token,

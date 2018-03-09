fcec2ff04b6e457d50bbcd90a749535c94d2c716
Jackson <wupeng_xg@163.com>
2018-03-09 21:58:37 +0800

    feat: 增加 mip-story 组件 (#1048)
    * feat: 增加 mip-story 组件
    
    * feat: 增加 mip-story 组件
    
    * feat: 增加 mip-story 组件

--- src/mip-story/mip-story-hint.js
+++ src/mip-story/mip-story-hint.js
@@ -8,6 +8,7 @@ define(function (require) {
 
     var MIP_STORY_HINT_DAMPING_HIDE = 'mip-story-hint-damping-hide';
     var MIP_STORY_SYSTEM_SHOW = 'mip-story-system-show';
+    var HASSHOWMIPSTORYHINT = 'has-show-mip-story-hint';
     var FIRST_PAGE_NAVIGATION_OVERLAY_TIMEOUT = 250;
 
     var util = require('util');
@@ -35,6 +36,16 @@ define(function (require) {
             +   '    <mip-img src="https://mip-extensions.bj.bcebos.com/mip-story/mip-story-rotate.png"></mip-img>'
             +   '    <p>为了更好的体验，请将手机横过来</p>'
             +   '</div>'
+            +   '<div class="mip-story-page-switch">'
+            +   '   <span class="mip-story-page-switch-left">'
+            +   '     <span></span>'
+            +   '     <span></span>'
+            +   '   </span>'
+            +   '   <span class="mip-story-page-switch-right">'
+            +   '       <span></span>'
+            +   '       <span></span>'
+            +   '   </span>'
+            +   '</div>'
             + '</aside>';
         return html;
     };
@@ -49,22 +60,71 @@ define(function (require) {
         }, FIRST_PAGE_NAVIGATION_OVERLAY_TIMEOUT);
     };
 
-    MIPStoryHint.prototype.hideDamping = function () {
+    MIPStoryHint.prototype.hideDamping = function () {        
         var ele = document.querySelector('.mip-story-hint');
         util.css(ele, {display: 'none'});
         ele.classList.add(MIP_STORY_HINT_DAMPING_HIDE);
     };
 
     MIPStoryHint.prototype.showSystemLater = function () {
-        var ele = document.querySelector('.mip-story-hint');
-        util.css(ele, {display: 'block'});
-        ele.classList.add(MIP_STORY_SYSTEM_SHOW);
-    };
+        var hasShown = localStorage.getItem(HASSHOWMIPSTORYHINT);
+        if (!hasShown) {
+            var ele = document.querySelector('.mip-story-hint');
+            util.css(ele, {display: 'block'});
+            ele.classList.add(MIP_STORY_SYSTEM_SHOW);
+            localStorage.setItem('has-show-mip-story-hint', '1');
+        }
+    };    
 
     MIPStoryHint.prototype.hideSystemLater = function () {
         var ele = document.querySelector('.mip-story-hint');
         util.css(ele, {display: 'none'});
         ele.classList.remove(MIP_STORY_SYSTEM_SHOW);
+    };
+
+    MIPStoryHint.prototype.toggleSystemLater = function () {        
+        var ele = document.querySelector('.mip-story-hint');
+        var display = ele.style.display;
+        if (display === 'block') {
+            this.hideSystemLater();
+        }
+        else {
+            this.showSystemLater();
+        }
+    };
+    
+    MIPStoryHint.prototype.showPageSwitchLayer = function () {
+        var hint = document.querySelector('.mip-story-hint');
+        util.css(hint, {display: 'block'});
+        var sw = document.querySelector('.mip-story-page-switch');
+        util.css(sw, {display: 'block'});
+        var left = document.querySelector('.mip-story-page-switch-left');
+        util.css(left, {display: 'none'});
+        var right = document.querySelector('.mip-story-page-switch-right');
+        util.css(right, {display: 'block'});
+        setTimeout(function () {
+            util.css(hint, {display: 'none'});
+            util.css(sw, {display: 'none'});
+            util.css(left, {display: 'none'});
+            util.css(right, {display: 'none'});
+        }, 400);
+    };
+    
+    MIPStoryHint.prototype.hidePageSwitchLayer = function () {
+        var hint = document.querySelector('.mip-story-hint');
+        util.css(hint, {display: 'block'});
+        var sw = document.querySelector('.mip-story-page-switch');
+        util.css(sw, {display: 'block'});
+        var left = document.querySelector('.mip-story-page-switch-left');
+        util.css(left, {display: 'block'});
+        var right = document.querySelector('.mip-story-page-switch-right');
+        util.css(right, {display: 'none'});
+        setTimeout(function () {
+            util.css(hint, {display: 'none'});
+            util.css(sw, {display: 'none'});
+            util.css(left, {display: 'none'});
+            util.css(right, {display: 'none'});
+        }, 400);
     };
 
     return MIPStoryHint;

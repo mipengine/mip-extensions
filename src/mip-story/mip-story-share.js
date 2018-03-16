2fbd66d96a62f4c06946fee7bce11538535b1a18
wupengFEX <smartfutureplayer@gmail.com>
2018-03-16 23:29:55 +0800

    添加分享地址

--- src/mip-story/mip-story-share.js
+++ src/mip-story/mip-story-share.js
@@ -6,6 +6,7 @@
 define(function (require) {
     'use strict';
 
+    var util = require('util');
     var MIP_STORY_SHARE_SHOW = 'mip-story-share-show';
 
     function MIPStoryHint() {
@@ -18,9 +19,10 @@ define(function (require) {
                 data: ['_trackEvent', '小故事分享取消', '点击', window.location.href]
             })
         );
+        var shareUrl = util.parseCacheUrl(location.href);
         var html = '<aside class="mip-story-share">'
             +   '<div class="mip-share-container">'
-            +   '    <mip-share></mip-share>'
+            +   '    <mip-share url="' + shareUrl + '"></mip-share>'
             +   '</div>'
             +   '<span class="mip-story-share-cancel" data-stats-baidu-obj="' + shareCancelStats + '">取消</span>'
             + '</aside>';

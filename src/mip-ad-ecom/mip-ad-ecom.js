f1b224cc6fc78101e9c17c6295b2a2f7ecf914a8
jennyliang220 <jiaojiaomao220@163.com>
2018-05-08 14:00:05 +0800

    feat: 增加组件ab实验readme

---
+++ src/mip-ad-ecom/mip-ad-ecom.js
@@ -0,0 +1,19 @@
+/**
+ * @file mip-ad-ecom 组件
+ * @author
+ */
+
+define(function (require) {
+    'use strict';
+
+    var customElement = require('customElement').create();
+
+    /**
+     * 第一次进入可视区回调，只会执行一次
+     */
+    customElement.prototype.firstInviewCallback = function () {
+        // TODO
+    };
+
+    return customElement;
+});

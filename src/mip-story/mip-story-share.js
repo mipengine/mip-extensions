d0c8e1669e72632e4daa0f8d43561d92efba4c6d
Espoir-L <413308430@qq.com>
2018-06-19 15:10:29 +0800

    fix：mip-story 在简搜下分享的问题 (#1194)

--- src/mip-story/mip-story-share.js
+++ src/mip-story/mip-story-share.js
@@ -48,9 +48,7 @@ define(function (require) {
     MIPStoryShare.prototype.showShareLayer = function () {
         var scSupport = this.supportCraft();
         // 适配简单搜索，简单没有给出单独调用微信等渠道的api, 所以在这里拦截一下；
-        // 如果简搜支持且为百度域的可吊起简搜，由于简搜会对非百度域的分享做特殊处理；
-        var hostName = util.parseCacheUrl(location.hostname);
-        if (scSupport.support && hostName.indexOf('baidu.com') !== -1) {
+        if (scSupport.support) {
             this.callSearchCraftShare(scSupport.os);
             return;
         }
@@ -97,7 +95,9 @@ define(function (require) {
         var craft = /SearchCraft/i.test(shareUa);
         var shareosAndroid = /Android/i.test(shareUa);
         var shareV = craft && shareUa.match(/SearchCraft\/([\d.]*)/);
-        var supportAnd = ((shareosAndroid && parseFloat(shareV[1]) > 1.5));
+        var hostName = util.parseCacheUrl(location.hostname);
+        // 在安卓机型内，如果简搜支持且为百度域的可吊起简搜，由于简搜会对非百度域的分享做特殊处理；
+        var supportAnd = ((shareosAndroid && parseFloat(shareV[1]) > 1.5) && hostName.indexOf('baidu.com') !== -1);
         var supportIos = (!shareosAndroid && (parseFloat(shareV[1])) > 1.11);
         var support = craft && (supportAnd || supportIos);
         return {

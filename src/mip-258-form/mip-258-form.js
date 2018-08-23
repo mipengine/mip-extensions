1f480efd2334be63536b1c0a8e1122bf1fcd5270
jiangnan <232957726@qq.com>
2018-08-23 13:48:39 +0800

    新增组件

--- src/mip-258-form/mip-258-form.js
+++ src/mip-258-form/mip-258-form.js
@@ -13,47 +13,7 @@ define(function (require) {
         var addClearBtn = element.hasAttribute('clear');
         form.createDom(element);
 
-        //获取验证码
-        $(element).find('.btnCode').click(function(){
-            let url = $(this).attr('btn-url');
-            let mobile = $('#inquiryMobile').val();
-            getInquiryMobileCode(mobile,60,url)
-        })
-
-        //获取询价手机验证码
-        function getInquiryMobileCode(mobile,second,url){
-            if(mobile==''||isNaN(mobile)){
-                alert('请输入手机号码');
-                return false;
-            }else if(mobile.length!=11){
-                alert('请输入正确的手机号码');
-                return false;
-            }
-            if(second<=1){
-                $("#waitInquiry").html(59);
-                $("#getInquiryCode").show();
-                $("#waitInquiryCode").hide();
-                return true;
-            }
-            if(second >= 60){
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: 'mobile='+mobile,
-                    async:true,
-                    success: function(data) {
-                        show(data.info);
-                    }
-                });
-                $("#getInquiryCode").hide();
-                $("#waitInquiryCode").show();
-            }
-            second--;
-            $("#waitInquiry").html(second);
-            setTimeout(function() {
-                getInquiryMobileCode(mobile,second)
-            },1000);
-        };
+       
     };
     return customElement;
 });

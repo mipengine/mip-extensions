3ec11f95106ca6b3dd6a2c5dc9c870f2f15a04d4
jiangnan <232957726@qq.com>
2018-08-23 13:50:08 +0800

    新增组件

--- src/mip-258-form/mip-258-form.js
+++ src/mip-258-form/mip-258-form.js
@@ -10,10 +10,42 @@ define(function (require) {
 
     customElement.prototype.build = function () {
         var element = this.element;
-        var addClearBtn = element.hasAttribute('clear');
-        form.createDom(element);
-
-       
+        //获取验证码
+      
+        //获取询价手机验证码
+        function getInquiryMobileCode(mobile,second,url){
+            if(mobile==''||isNaN(mobile)){
+                alert('请输入手机号码');
+                return false;
+            }else if(mobile.length!=11){
+                alert('请输入正确的手机号码');
+                return false;
+            }
+            if(second<=1){
+                $("#waitInquiry").html(59);
+                $("#getInquiryCode").show();
+                $("#waitInquiryCode").hide();
+                return true;
+            }
+            if(second >= 60){
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: 'mobile='+mobile,
+                    async:true,
+                    success: function(data) {
+                        show(data.info);
+                    }
+                });
+                $("#getInquiryCode").hide();
+                $("#waitInquiryCode").show();
+            }
+            second--;
+            $("#waitInquiry").html(second);
+            setTimeout(function() {
+                getInquiryMobileCode(mobile,second)
+            },1000);
+        };
     };
     return customElement;
 });

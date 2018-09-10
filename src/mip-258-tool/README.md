dc400292a31e1a1c3a624c788e12df6c60a0cea4
陈威林 <chenweilin@258.com>
2018-09-10 11:37:33 +0800

    redirect

--- src/mip-258-tool/README.md
+++ src/mip-258-tool/README.md
@@ -11,7 +11,7 @@ ajax请求
 ### 基本使用
 
 ```html
-<button type="button" on="tap:258-tool.alert(1233)">测试</button>
+<button type="button" on="tap:258-tool.redirect(http://www.baidu.com,2000)">测试</button>
 <mip-258-tool id="258-tool"></mip-258-tool>
 ```
 ### on
@@ -19,6 +19,7 @@ ajax请求
 说明：goback事件对应window.history.back()事件  
 reload(100)事件对应window.reload()事件，传入毫秒数，用于刷新等待  
 alert()事件对应window.alert()事件  
+redirect('url,time_wait') 对应window.location.href事件
 必选项: 否  
 
 
--- src/mip-258-tool/mip-258-tool.js
+++ src/mip-258-tool/mip-258-tool.js
@@ -13,6 +13,7 @@ define(function (require) {
     };
 
     customElement.prototype.firstInviewCallback = function (event, str) {
+
         // 添加页面刷新事件
         this.addEventAction('reload', function (event, str) {
             str = str ? 500 : parseInt(str, 10);
@@ -20,15 +21,31 @@ define(function (require) {
                 window.top.location.reload();
             }, str);
         });
+
         // 添加alert提示事件
         this.addEventAction('alert', function (event, str) {
             window.top.alert(str);
         });
+
+        // 添加后退事件
         this.addEventAction('goback', function (event, str) {
             if (window.top.history.length === 0) {
                 window.top.location.href = str;
             }
             window.top.history.back();
+        });
+
+        // 添加重定向事件
+        this.addEventAction('redirect', function (event, url) {
+            var params = url.split(',');
+            url = params[0];
+            var timeWait = params[1] ? Number.parseInt(params[1], 10) : 0;
+
+            if (url) {
+                setTimeout(function () {
+                    window.top.location.href = url;
+                }, timeWait);
+            }
         });
     };
     return customElement;

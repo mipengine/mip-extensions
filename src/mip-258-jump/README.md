934fb2385e9ac9751d359179c02235302b338d0c
江南 <jiangnan@258.com>
2019-01-15 09:15:42 +0800

    更新

---
+++ src/mip-258-jump/README.md
@@ -0,0 +1,23 @@
+# mip-258-jump
+
+mip-258-jump 跳转组件
+
+标题|内容
+----|----
+类型|通用
+支持布局|fixed
+所需脚本|https://c.mipcdn.com/static/v1/mip-258-jump/mip-258-jump.js
+
+## 示例
+### 基本使用
+
+```html
+<mip-258-jump></mip-258-jump>
+```
+## 属性
+
+### method
+
+说明：自动跳转www或者m  
+必选项：是 
+
---
+++ src/mip-258-jump/mip-258-jump.js
@@ -0,0 +1,41 @@
+/**
+ * @file mip-back 组件
+ * @author
+ */
+
+define(function (require) {
+    var customElement = require('customElement').create();
+    var $ = require('zepto');
+
+    /**
+    * 第一次进入可视区回调，只会执行一次
+    */
+    customElement.prototype.firstInviewCallback = function () {
+        // TODO
+        function ispc() {
+            var userAgentInfo = navigator.userAgent;
+            var Agents = [
+                'Android',
+                'iPhone',
+                'SymbianOS',
+                'Windows Phone',
+                'iPad',
+                'iPod'
+            ];
+            var flag = true;
+            for (var v = 0; v < Agents.length; v++) {
+                if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+        if (ispc()) {
+            if (location.href.indexOf('//m.') !== -1) {
+                window.top.location.href = window.top.location.href.replace('m.', 'www.');
+            }
+        }
+    };
+    return customElement;
+});
